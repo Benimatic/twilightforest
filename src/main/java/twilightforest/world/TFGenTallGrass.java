5e8dfda9c8fe85a2281d1da054cc81922f44ef8b
Vincent Lee <vincent.lee@outlook.com>
2016-07-03 12:19:06 -0500

    Expand role of TFGenTallGrass

--- src/main/java/twilightforest/world/TFGenTallGrass.java
+++ src/main/java/twilightforest/world/TFGenTallGrass.java
@@ -2,45 +2,40 @@ package twilightforest.world;
 
 import java.util.Random;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
 
+// [VanillaCopy] of WorldGenTallGrass, relevant changes noted
 public class TFGenTallGrass extends WorldGenerator {
 
-	private Block tallGrassID;
-	private int tallGrassMetadata;
-	private int amount;
+    private final IBlockState state;
+	private final int amount;
 
-	public TFGenTallGrass(Block blockID, int meta, int amount) {
-		this.amount = amount;
-		this.tallGrassID = blockID;
-		this.tallGrassMetadata = meta;
+	public TFGenTallGrass(IBlockState state, int amount) { // TF - Allow any blockstate, allow amount to be changed
+		this.state = state;
+        this.amount = amount;
 	}
 
-    public boolean generate(World par1World, Random par2Random, int x, int y, int z)
+    @Override
+    public boolean generate(World worldIn, Random rand, BlockPos position)
     {
-        Block block = null;
-        do 
+        do
         {
-            block = par1World.getBlock(x,  y, z);
-            if (block != null && !block.isLeaves(par1World, x, y, z))
-            {
-                break;
-            }
-            y--;
-        } while (y > 0);
+            IBlockState state = worldIn.getBlockState(position);
+            if (!state.getBlock().isAir(state, worldIn, position) && !state.getBlock().isLeaves(state, worldIn, position)) break;
+            position = position.down();
+        } while (position.getY() > 0);
 
-        
-        for (int i = 0; i < amount; ++i)
+        for (int i = 0; i < amount; ++i) // TF - 128 -> amount
         {
-            int dx = x + par2Random.nextInt(8) - par2Random.nextInt(8);
-            int dy = y + par2Random.nextInt(4) - par2Random.nextInt(4);
-            int dz = z + par2Random.nextInt(8) - par2Random.nextInt(8);
+            BlockPos blockpos = position.add(rand.nextInt(8) - rand.nextInt(8), rand.nextInt(4) - rand.nextInt(4), rand.nextInt(8) - rand.nextInt(8));
 
-            if (par1World.isAirBlock(dx, dy, dz) && this.tallGrassID.canBlockStay(par1World, dx, dy, dz))
+            // TF - use our own state
+            if (worldIn.isAirBlock(blockpos) && state.getBlock().canBlockStay(worldIn, blockpos, state))
             {
-                par1World.setBlock(dx, dy, dz, this.tallGrassID, this.tallGrassMetadata, 2);
+                worldIn.setBlockState(blockpos, state, 2);
             }
         }
 
