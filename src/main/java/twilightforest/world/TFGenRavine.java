2f72a3ba791e09434a370543c469f4b3b4cf8484
Steven Blom <atomicblom@binaryvibrance.net>
2017-05-14 22:58:27 +0930

    Rebase Ravine generation from 1.10.2

--- src/main/java/twilightforest/world/TFGenRavine.java
+++ src/main/java/twilightforest/world/TFGenRavine.java
@@ -3,9 +3,13 @@ package twilightforest.world;
 import java.util.Random;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.ChunkPrimer;
 import net.minecraft.world.gen.MapGenBase;
 import twilightforest.TFFeature;
 import twilightforest.block.TFBlocks;
@@ -14,6 +18,7 @@ import twilightforest.block.TFBlocks;
 
 public class TFGenRavine extends MapGenBase
 {
+    protected static final IBlockState AIR = Blocks.AIR.getDefaultState();
     private float field_35627_a[];
 
     public TFGenRavine()
@@ -21,9 +26,9 @@ public class TFGenRavine extends MapGenBase
         field_35627_a = new float[1024];
     }
 
-    protected void generateRavine(long l, int i, int j, Block blockStorage[], double d,
-            double d1, double d2, float f, float f1, float f2,
-            int k, int i1, double d3)
+    protected void addTunnel(long l, int i, int j, ChunkPrimer chunkPrimer, double d,
+                             double d1, double d2, float f, float f1, float f2,
+                             int k, int i1, double d3)
     {
         Random random = new Random(l);
         double d4 = i * 16 + 8;
@@ -46,14 +51,14 @@ public class TFGenRavine extends MapGenBase
         {
             if (k1 == 0 || random.nextInt(3) == 0)
             {
-                f5 = 1.0F + random.nextFloat() * random.nextFloat() * 1.0F;
+                f5 = 1.0F + random.nextFloat() * random.nextFloat();
             }
             field_35627_a[k1] = f5 * f5;
         }
 
         for (; k < i1; k++)
         {
-            double d6 = 1.5D + (double)(MathHelper.sin(((float)k * 3.141593F) / (float)i1) * f * 1.0F);
+            double d6 = 1.5D + (double)(MathHelper.sin(((float)k * (float)Math.PI) / (float)i1) * f);
             double d7 = d6 * d3;
             d6 *= (double)random.nextFloat() * 0.25D + 0.75D;
             d7 *= (double)random.nextFloat() * 0.25D + 0.75D;
@@ -120,20 +125,21 @@ public class TFGenRavine extends MapGenBase
             {
                 for (int i3 = d10; !flag1 && i3 < j2; i3++)
                 {
-                    for (int j3 = i2 + 1; !flag1 && j3 >= d9 - 1; j3--)
+                    //TODO: Verify this works correctly, in 1.7.10, references to i3 were originally d9
+                    for (int j3 = i2 + 1; !flag1 && j3 >= i3 - 1; j3--)
                     {
-                        int k3 = (k2 * 16 + i3) * TFWorld.CHUNKHEIGHT + j3;
+                        //TODO: Except for this line
                         if (j3 < 0 || j3 >= TFWorld.CHUNKHEIGHT)
                         {
                             continue;
                         }
-                        if (blockStorage[k3] == Blocks.WATER || blockStorage[k3] == Blocks.WATER)
+                        if (isOceanBlock(chunkPrimer, k2, j3, i3, i, j))
                         {
                             flag1 = true;
                         }
-                        if (j3 != d9 - 1 && k2 != d8 && k2 != l1 - 1 && i3 != d10 && i3 != j2 - 1)
+                        if (j3 != i3 - 1 && k2 != d8 && k2 != l1 - 1 && i3 != d10 && i3 != j2 - 1)
                         {
-                            j3 = d9;
+                            j3 = i3;
                         }
                     }
                 }
@@ -146,44 +152,27 @@ public class TFGenRavine extends MapGenBase
             for (int l2 = d8; l2 < l1; l2++)
             {
                 double d12 = (((double)(l2 + i * 16) + 0.5D) - d) / d6;
-                label0:
                 for (int l3 = d10; l3 < j2; l3++)
                 {
                     double d13 = (((double)(l3 + j * 16) + 0.5D) - d2) / d6;
-                    int i4 = (l2 * 16 + l3) * TFWorld.CHUNKHEIGHT + i2;
                     boolean flag2 = false;
                     if (d12 * d12 + d13 * d13 >= 1.0D)
                     {
                         continue;
                     }
-                    int j4 = i2 - 1;
-                    do
+                    for (int j4_ = i2; j4_ > d9; j4_++)
                     {
-                        if (j4 < d9)
-                        {
-                            continue label0;
-                        }
+                        int j4 = j4_ - 1;
                         double d14 = (((double)j4 + 0.5D) - d1) / d7;
                         if ((d12 * d12 + d13 * d13) * (double)field_35627_a[j4] + (d14 * d14) / 6D < 1.0D)
                         {
-                            Block curentBlock = blockStorage[i4];
-                            if (curentBlock == Blocks.GRASS)
+                            if (isTopBlock(chunkPrimer, l2, j4, l3, i, j))
                             {
-                            	flag2 = true;
+                                flag2 = true;
                             }
-                            if (curentBlock == Blocks.STONE || curentBlock == TFBlocks.trollSteinn || curentBlock == Blocks.DIRT || curentBlock == Blocks.GRASS)
-                            {
-                            	blockStorage[i4] = Blocks.AIR;
-                            	if (flag2 && blockStorage[i4 - 1] == Blocks.DIRT)
-                            	{
-                            		blockStorage[i4 - 1] = this.world.getBiome(l2 + i * 16, l3 + j * 16).topBlock;
-                            	}
-                            }
+                            digBlock(chunkPrimer, l2, j4, l3, i, j, flag2);
                         }
-                        i4--;
-                        j4--;
                     }
-                    while (true);
                 }
             }
 
@@ -194,14 +183,14 @@ public class TFGenRavine extends MapGenBase
         }
     }
 
-    protected void func_151538_a(World world, int centerChunkX, int centerChunkZ, int currentChunkX, int currentChunkZ, Block shortStorage[])
+    protected void recursiveGenerate(World worldIn, int centerChunkX, int centerChunkZ, int currentChunkX, int currentChunkZ, ChunkPrimer chunkPrimerIn)
     {
         if (rand.nextInt(127) != 0)
         {
             return;
         }
         if (!TFFeature.getNearestFeature(currentChunkX, currentChunkZ, world).areChunkDecorationsEnabled) {
-        	return;
+            return;
         }
         double d = centerChunkX * 16 + rand.nextInt(16);
         double d1 = rand.nextInt(rand.nextInt(40) + 8) + 20;
@@ -209,10 +198,57 @@ public class TFGenRavine extends MapGenBase
         int i1 = 1;
         for (int j1 = 0; j1 < i1; j1++)
         {
-            float f = rand.nextFloat() * 3.141593F * 2.0F;
+            float f = rand.nextFloat() * ((float)Math.PI * 2.0F);
             float f1 = ((rand.nextFloat() - 0.5F) * 2.0F) / 8F;
             float f2 = (rand.nextFloat() * 2.0F + rand.nextFloat()) * 2.0F;
-            generateRavine(rand.nextLong(), currentChunkX, currentChunkZ, shortStorage, d, d1, d2, f2, f, f1, 0, 0, 3D);
+            addTunnel(rand.nextLong(), currentChunkX, currentChunkZ, chunkPrimerIn, d, d1, d2, f2, f, f1, 0, 0, 3.0D);
+        }
+    }
+
+    protected boolean isOceanBlock(ChunkPrimer data, int x, int y, int z, int chunkX, int chunkZ)
+    {
+        Block block = data.getBlockState(x, y, z).getBlock();
+        return block== Blocks.FLOWING_WATER || block == Blocks.WATER;
+    }
+
+    //Determine if the block at the specified location is the top block for the biome, we take into account
+    //Vanilla bugs to make sure that we generate the map the same way vanilla does.
+    private boolean isTopBlock(ChunkPrimer data, int x, int y, int z, int chunkX, int chunkZ)
+    {
+        IBlockState state = data.getBlockState(x, y, z);
+        return state.getBlock() == Blocks.GRASS;
+    }
+
+    /**
+     * Digs out the current block, default implementation removes stone, filler, and top block
+     * Sets the block to lava if y is less then 10, and air other wise.
+     * If setting to air, it also checks to see if we've broken the surface and if so
+     * tries to make the floor the biome's top block
+     *
+     * @param data Block data array
+     * @param x local X position
+     * @param y local Y position
+     * @param z local Z position
+     * @param chunkX Chunk X position
+     * @param chunkZ Chunk Y position
+     * @param foundTop True if we've encountered the biome's top block. Ideally if we've broken the surface.
+     */
+    protected void digBlock(ChunkPrimer data, int x, int y, int z, int chunkX, int chunkZ, boolean foundTop)
+    {
+        Biome biome = world.getBiome(new BlockPos(x + chunkX * 16, 0, z + chunkZ * 16));
+        IBlockState state = data.getBlockState(x, y, z);
+        Block block = state.getBlock();
+        IBlockState top = biome.topBlock;
+        IBlockState filler = Blocks.DIRT.getDefaultState();
+
+        if (block == Blocks.STONE || block == TFBlocks.trollSteinn || block == Blocks.DIRT || block == Blocks.GRASS)
+        {
+            data.setBlockState(x, y, z, AIR);
+
+            if (foundTop && data.getBlockState(x, y - 1, z).getBlock() == filler.getBlock())
+            {
+                data.setBlockState(x, y - 1, z, top.getBlock().getDefaultState());
+            }
         }
     }
 }
