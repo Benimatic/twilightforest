5c22c55d5aafabecfa555c5931977dee522efe47
Steven Blom <atomicblom@binaryvibrance.net>
2017-05-22 11:34:45 +0930

    Missing ObjectHolder annotation

--- src/main/java/twilightforest/item/TFItems.java
+++ src/main/java/twilightforest/item/TFItems.java
@@ -12,6 +12,7 @@ import net.minecraftforge.common.util.EnumHelper;
 import twilightforest.TwilightForestMod;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
+@GameRegistry.ObjectHolder(TwilightForestMod.ID)
 public class TFItems {
 
     public static ItemArmor.ArmorMaterial ARMOR_NAGA = EnumHelper.addArmorMaterial("NAGA_SCALE", "naga_scale", 21, new int[] {2, 7, 6, 3}, 15, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0f);
