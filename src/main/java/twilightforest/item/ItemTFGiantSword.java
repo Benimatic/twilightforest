330c2508a7a75e45926e64bb38674e1c73c74c06
Vincent Lee <vincent.lee@outlook.com>
2017-05-21 20:28:05 -0500

    More random items

--- src/main/java/twilightforest/item/ItemTFGiantSword.java
+++ src/main/java/twilightforest/item/ItemTFGiantSword.java
@@ -3,8 +3,9 @@ package twilightforest.item;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import twilightforest.client.ModelRegisterCallback;
 
-public class ItemTFGiantSword extends ItemSword {
+public class ItemTFGiantSword extends ItemSword implements ModelRegisterCallback {
 
 	public ItemTFGiantSword(Item.ToolMaterial par2EnumToolMaterial) {
 		super(par2EnumToolMaterial);
