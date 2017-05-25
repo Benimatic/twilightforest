e858f9b575b7cfebd2ce6d095d0f7a4a7657e26b
Vincent Lee <vincent.lee@outlook.com>
2017-05-24 21:39:52 -0500

    Particle needs to be defined separately to work properly

--- src/main/java/twilightforest/block/BlockTFFireflyJar.java
+++ src/main/java/twilightforest/block/BlockTFFireflyJar.java
@@ -20,7 +20,7 @@ import twilightforest.item.TFItems;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockTFFireflyJar extends Block {
+public class BlockTFFireflyJar extends Block implements{
 
     private static final AxisAlignedBB AABB = new AxisAlignedBB(0.1875F, 0.0F, 0.1875F, 0.8125F, 1.0F, 0.8125F);
 
