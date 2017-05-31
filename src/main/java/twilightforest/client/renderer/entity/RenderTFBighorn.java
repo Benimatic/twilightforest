d942e22e9a4347b7cf4c2b8f5b5be3889a6ebe9b
Benimatic <ben.mazur@gmail.com>
2017-05-31 19:44:59 -0400

    Shamelessly inherit RenderBighorn from RenderSheep and use their layers

--- src/main/java/twilightforest/client/renderer/entity/RenderTFBighorn.java
+++ src/main/java/twilightforest/client/renderer/entity/RenderTFBighorn.java
@@ -4,22 +4,25 @@ import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderSheep;
+import net.minecraft.client.renderer.entity.layers.LayerSheepWool;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.EntitySheep;
 import net.minecraft.util.ResourceLocation;
 import twilightforest.TwilightForestMod;
 import twilightforest.entity.passive.EntityTFBighorn;
 
-public class RenderTFBighorn extends RenderLiving<EntityTFBighorn> {
+public class RenderTFBighorn extends RenderSheep {
 	private static final ResourceLocation textureLoc = new ResourceLocation(TwilightForestMod.MODEL_DIR + "bighorn.png");
 
 	public RenderTFBighorn(RenderManager manager, ModelBase baseModel, ModelBase coatModel, float shadowSize) {
-		super(manager, baseModel, shadowSize);
-		// TODO layer renderer for coat
+		super(manager);
+		this.shadowSize = shadowSize;
+		this.mainModel = baseModel;
+		this.addLayer(new LayerSheepWool(this));
 	}
 
 	@Override
-    protected ResourceLocation getEntityTexture(EntityTFBighorn ent)
+    protected ResourceLocation getEntityTexture(EntitySheep ent)
     {
         return textureLoc;
     }
