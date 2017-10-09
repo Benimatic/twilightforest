e0a2e311b915f209fb6c97b35453f95c9fe75fb6
Drullkus <ericsplanet@mac.com>
2017-10-08 22:43:46 -0700

    Final touchups, are we ready for release?

--- src/main/java/twilightforest/block/BlockTFTrophyPedestal.java
+++ src/main/java/twilightforest/block/BlockTFTrophyPedestal.java
@@ -6,14 +6,12 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.properties.PropertyDirection;
-import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.SoundEvents;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
@@ -24,11 +22,8 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import twilightforest.TFAdvancements;
 import twilightforest.TwilightForestMod;
-import twilightforest.block.enums.BossVariant;
 import twilightforest.client.ModelRegisterCallback;
 import twilightforest.item.TFItems;
-import twilightforest.tileentity.TileEntityTFTrophy;
-import twilightforest.util.PlayerHelper;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
@@ -36,7 +31,6 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @ParametersAreNonnullByDefault
 public class BlockTFTrophyPedestal extends Block implements ModelRegisterCallback {
 	public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
-	public static final PropertyEnum<BossVariant> BOSS = PropertyEnum.create("boss", BossVariant.class, BossVariant::hasTrophy);
 	public static final PropertyBool LATENT = PropertyBool.create("latent");
 
 	private static final AxisAlignedBB AABB = new AxisAlignedBB(0.0625F, 0.0F, 0.0625F, 0.9375F, 1.0F, 0.9375F);
@@ -47,12 +41,12 @@ public class BlockTFTrophyPedestal extends Block implements ModelRegisterCallbac
 		this.setResistance(2000.0F);
 		this.setSoundType(SoundType.STONE);
 		this.setCreativeTab(TFItems.creativeTab);
-		this.setDefaultState(getDefaultState().withProperty(LATENT, true).withProperty(FACING, EnumFacing.NORTH).withProperty(BOSS, BossVariant.NAGA));
+		this.setDefaultState(getDefaultState().withProperty(LATENT, true).withProperty(FACING, EnumFacing.NORTH));
 	}
 
 	@Override
 	protected BlockStateContainer createBlockState() {
-		return new BlockStateContainer(this, FACING, LATENT, BOSS);
+		return new BlockStateContainer(this, FACING, LATENT);
 	}
 
 	@Override
@@ -73,30 +67,6 @@ public class BlockTFTrophyPedestal extends Block implements ModelRegisterCallbac
 			ret = ret.withProperty(LATENT, true);
 		}
 		return ret;
-	}
-
-	@Override
-	@Deprecated
-	public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos pos) {
-		IBlockState stateAbove = world.getBlockState(pos.up());
-		TileEntity tile = world.getTileEntity(pos);
-
-		if (stateAbove.getBlock() != TFBlocks.trophy || (!state.getValue(LATENT)) || tile == null || (!(tile instanceof TileEntityTFTrophy)))
-			return state;
-
-		TileEntityTFTrophy trophy = (TileEntityTFTrophy) tile;
-
-		BossVariant variant;
-
-		switch (trophy.getSkullType()) {
-			case 0:
-				variant = BossVariant.HYDRA;
-				break;
-			default:
-				variant = BossVariant.UR_GHAST;
-		}
-
-		return state.withProperty(BOSS, variant);
 	}
 
 	@Override
