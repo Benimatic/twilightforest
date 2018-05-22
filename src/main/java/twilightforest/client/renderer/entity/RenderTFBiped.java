b3cb851979a86d08b7aed7c91eddbfb902eb2c8b
Andromander <cathy@feodoroff.com>
2018-05-22 13:06:18 +1000

    Fix Redcap boot rendering. (#515)

--- src/main/java/twilightforest/client/renderer/entity/RenderTFBiped.java
+++ src/main/java/twilightforest/client/renderer/entity/RenderTFBiped.java
@@ -3,6 +3,7 @@ package twilightforest.client.renderer.entity;
 import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.layers.LayerBipedArmor;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.ResourceLocation;
 import twilightforest.TwilightForestMod;
@@ -12,6 +13,7 @@ public class RenderTFBiped<T extends EntityLiving> extends RenderBiped<T> {
 
 	public RenderTFBiped(RenderManager manager, ModelBiped modelBiped, float scale, String textureName) {
 		super(manager, modelBiped, scale);
+		this.addLayer(new LayerBipedArmor(this));
 
 		if (textureName.startsWith("textures")) {
 			textureLoc = new ResourceLocation(textureName);
