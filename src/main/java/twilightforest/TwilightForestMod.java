13157dc1cb0081bd1e18dd7ca7b8915524a35af0
Ben Staddon <quadraxis@hotmail.co.uk>
2018-06-13 21:26:43 +0100

    Update Sponge compatibility note

--- src/main/java/twilightforest/TwilightForestMod.java
+++ src/main/java/twilightforest/TwilightForestMod.java
@@ -71,10 +71,10 @@ public class TwilightForestMod {
 	@SuppressWarnings("unused")
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-		if (Loader.isModLoaded("sponge"))
+		if (Loader.isModLoaded("sponge")) {
 			LOGGER.info("It looks like you have Sponge installed! You may notice Hydras spawning incorrectly with floating heads.\n" +
-					"Open the `config/sponge/global.conf` file, and set the `max-bounding-box-size` to `6000` or higher, or `0`.\n" +
-					"Sponge's default maximum bounding box is too low for the Hydra. Have fun!");
+					"If so, please update Sponge to resolve this issue. Have fun!");
+		}
 
 		registerCreatures();
 		registerTileEntities();
