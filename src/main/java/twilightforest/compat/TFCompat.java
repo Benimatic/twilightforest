524e6384dfa9718062957330fe25c93539866cff
Drullkus <ericsplanet@mac.com>
2018-07-17 13:01:39 -0700

    Update Immersive Engineering dep

--- src/main/java/twilightforest/compat/TFCompat.java
+++ src/main/java/twilightforest/compat/TFCompat.java
@@ -74,16 +74,16 @@ public enum TFCompat {
         }
     }, // TODO Forestry
     IMMERSIVEENGINEERING("Immersive Engineering") {
-        @Override
-        protected boolean preInit() {
-            try {
-                VersionRange range = VersionRange.createFromVersionSpec("[0.12-83-407,)");
+        //@Override
+        //protected boolean preInit() {
+        //    try {
+        //        VersionRange range = VersionRange.createFromVersionSpec("[0.12-83-407,)");
 
-                return range.containsVersion(Loader.instance().getIndexedModList().get(this.name().toLowerCase(Locale.ROOT)).getProcessedVersion());
-            } catch (InvalidVersionSpecificationException e) {
-                return false;
-            }
-        }
+        //        return range.containsVersion(Loader.instance().getIndexedModList().get(this.name().toLowerCase(Locale.ROOT)).getProcessedVersion());
+        //    } catch (InvalidVersionSpecificationException e) {
+        //        return false;
+        //    }
+        //}
 
         @Override
         protected void initItems(TFRegisterItemEvent.ItemRegistryHelper items) {
