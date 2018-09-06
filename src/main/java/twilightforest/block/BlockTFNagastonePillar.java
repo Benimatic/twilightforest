c3165d630be1167f049881599aa7f4c531f9f433
Ben Staddon <quadraxis@hotmail.co.uk>
2018-09-07 00:30:15 +0100

    Make nagastone pillar blocks directional, closes #614

--- src/main/java/twilightforest/block/BlockTFNagastonePillar.java
+++ src/main/java/twilightforest/block/BlockTFNagastonePillar.java
@@ -1,11 +1,7 @@
 package twilightforest.block;
 
-import net.minecraft.block.BlockRotatedPillar;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -16,22 +12,18 @@ import twilightforest.item.TFItems;
 /**
  * Created by Drullkus on 12/12/17.
  */
-public class BlockTFNagastonePillar extends BlockRotatedPillar implements ModelRegisterCallback {
+public class BlockTFNagastonePillar extends BlockTFDirectionalRotatedPillar implements ModelRegisterCallback {
+
     protected BlockTFNagastonePillar() {
         super(Material.ROCK);
         this.setSoundType(SoundType.STONE);
         this.setCreativeTab(TFItems.creativeTab);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(AXIS, EnumFacing.Axis.Y));
+        this.setDefaultState(this.blockState.getBaseState().withProperty(AXIS, EnumFacing.Axis.Y).withProperty(REVERSED, false));
     }
 
     @SideOnly(Side.CLIENT)
     @Override
     public void registerModel() {
         ModelUtils.registerToState(this, 0, this.getDefaultState());
-    }
-
-    @Override
-    protected ItemStack getSilkTouchDrop(IBlockState state) {
-        return new ItemStack(Item.getItemFromBlock(this));
     }
 }
