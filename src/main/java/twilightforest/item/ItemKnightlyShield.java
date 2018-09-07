9940b79a109638f05b809c744ba5fab0479c83c0
Ben Staddon <quadraxis@hotmail.co.uk>
2018-09-07 08:22:29 +0100

    A few tweaks for the new shield

--- src/main/java/twilightforest/item/ItemKnightlyShield.java
+++ src/main/java/twilightforest/item/ItemKnightlyShield.java
@@ -1,6 +1,7 @@
 package twilightforest.item;
 
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemShield;
@@ -15,25 +16,28 @@ import javax.annotation.Nullable;
 import java.util.List;
 
 public class ItemKnightlyShield extends ItemShield implements ModelRegisterCallback {
+
     public ItemKnightlyShield() {
         this.setMaxDamage(1024);
         this.setCreativeTab(TFItems.creativeTab);
     }
 
+    @Override
     public String getItemStackDisplayName(ItemStack stack) {
-        return I18n.translateToLocal(this.getUnlocalizedNameInefficiently(stack) + ".name").trim();
+        return I18n.translateToLocal(this.getTranslationKey(stack) + ".name").trim();
     }
 
     @SideOnly(Side.CLIENT)
-    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-    }
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {}
 
-    public int getMaxItemUseDuration(ItemStack stack)
-    {
-        return 144000;
-    }
-
+    @Override
     public boolean getIsRepairable(ItemStack toRepair, ItemStack repair) {
         return repair.getItem() == TFItems.knightmetal_ingot || repair.getItem() != Item.getItemFromBlock(Blocks.PLANKS) && super.getIsRepairable(toRepair, repair);
+    }
+
+    @Override
+    public boolean isShield(ItemStack stack, @Nullable EntityLivingBase entity) {
+        return true;
     }
 }
