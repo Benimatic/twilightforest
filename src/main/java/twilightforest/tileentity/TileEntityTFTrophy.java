bb7e709dbfb61197740f210d54395b500a3156f5
Ben Staddon <quadraxis@hotmail.co.uk>
2018-09-11 20:28:20 +0100

    Fix trophy tile entities losing data

--- src/main/java/twilightforest/tileentity/TileEntityTFTrophy.java
+++ src/main/java/twilightforest/tileentity/TileEntityTFTrophy.java
@@ -1,6 +1,9 @@
 package twilightforest.tileentity;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntitySkull;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class TileEntityTFTrophy extends TileEntitySkull {
 
@@ -12,4 +15,8 @@ public class TileEntityTFTrophy extends TileEntitySkull {
 		this.ticksExisted++;
 	}
 
+	@Override
+	public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newState) {
+		return oldState.getBlock() != newState.getBlock();
+	}
 }
