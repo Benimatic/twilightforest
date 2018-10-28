c055945a4fc0b8d1baabbfc88fe4eef842777591
Ben Staddon <quadraxis@hotmail.co.uk>
2018-10-28 22:54:31 +0000

    Increase burn time for giant logs

--- src/main/java/twilightforest/item/ItemTFGiantBlock.java
+++ src/main/java/twilightforest/item/ItemTFGiantBlock.java
@@ -3,7 +3,6 @@ package twilightforest.item;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
@@ -14,8 +13,10 @@ import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import twilightforest.block.BlockTFGiantBlock;
+import twilightforest.block.TFBlocks;
 
 public class ItemTFGiantBlock extends ItemBlock {
+
 	public ItemTFGiantBlock(Block block) {
 		super(block);
 	}
@@ -57,5 +58,13 @@ public class ItemTFGiantBlock extends ItemBlock {
 		}
 
 		return EnumActionResult.SUCCESS;
+	}
+
+	@Override
+	public int getItemBurnTime(ItemStack itemStack) {
+		if (this.block == TFBlocks.giant_log) {
+			return 300 * 64;
+		}
+		return super.getItemBurnTime(itemStack);
 	}
 }
