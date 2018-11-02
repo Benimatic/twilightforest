bf795055e8a802eab2a54be984a1562ed263edda
Ben Staddon <quadraxis@hotmail.co.uk>
2018-11-02 23:55:48 +0000

    More event firing

--- src/main/java/twilightforest/util/EntityUtil.java
+++ src/main/java/twilightforest/util/EntityUtil.java
@@ -2,8 +2,10 @@ package twilightforest.util;
 
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.event.ForgeEventFactory;
 
 public class EntityUtil {
 
@@ -14,6 +16,8 @@ public class EntityUtil {
 	public static boolean canDestroyBlock(World world, BlockPos pos, IBlockState state, Entity entity) {
 		float hardness = state.getBlockHardness(world, pos);
 		return hardness >= 0f && hardness < 50f && !state.getBlock().isAir(state, world, pos)
-				&& state.getBlock().canEntityDestroy(state, world, pos, entity);
+				&& state.getBlock().canEntityDestroy(state, world, pos, entity)
+				&& (/* rude type limit */!(entity instanceof EntityLivingBase)
+				|| ForgeEventFactory.onEntityDestroyBlock((EntityLivingBase) entity, pos, state));
 	}
 }
