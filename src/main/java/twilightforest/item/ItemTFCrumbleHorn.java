d496f0c207467d517688898b6d7a0d02b2608864
Ben Staddon <quadraxis@hotmail.co.uk>
2018-11-16 02:02:06 +0000

    Expand possibilities for sheep-derived vandalism

--- src/main/java/twilightforest/item/ItemTFCrumbleHorn.java
+++ src/main/java/twilightforest/item/ItemTFCrumbleHorn.java
@@ -1,6 +1,7 @@
 package twilightforest.item;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockSand;
 import net.minecraft.block.BlockStoneBrick;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
@@ -17,20 +18,82 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.event.ForgeEventFactory;
+import org.apache.commons.lang3.tuple.Pair;
 import twilightforest.block.BlockTFMazestone;
+import twilightforest.block.BlockTFTowerWood;
+import twilightforest.block.BlockTFUnderBrick;
 import twilightforest.block.TFBlocks;
 import twilightforest.enums.MazestoneVariant;
+import twilightforest.enums.TowerWoodVariant;
+import twilightforest.enums.UnderBrickVariant;
 import twilightforest.util.WorldUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 public class ItemTFCrumbleHorn extends ItemTF {
 
 	private static final int CHANCE_HARVEST = 20;
 	private static final int CHANCE_CRUMBLE = 5;
 
+	private final List<Pair<Predicate<IBlockState>, UnaryOperator<IBlockState>>> crumbleTransforms = new ArrayList<>();
+	private final List<Predicate<IBlockState>> harvestedStates = new ArrayList<>();
+
 	protected ItemTFCrumbleHorn() {
 		this.setCreativeTab(TFItems.creativeTab);
 		this.maxStackSize = 1;
 		this.setMaxDamage(1024);
+		this.addCrumbleTransforms();
+	}
+
+	private void addCrumbleTransforms() {
+		addCrumble(() -> Blocks.STONE, () -> Blocks.COBBLESTONE.getDefaultState());
+		addCrumble(state -> state.getBlock() == Blocks.STONEBRICK
+						&& state.getValue(BlockStoneBrick.VARIANT) == BlockStoneBrick.EnumType.DEFAULT,
+				state -> state.withProperty(BlockStoneBrick.VARIANT, BlockStoneBrick.EnumType.CRACKED)
+		);
+		addCrumble(state -> state.getBlock() == TFBlocks.maze_stone
+						&& state.getValue(BlockTFMazestone.VARIANT) == MazestoneVariant.BRICK,
+				state -> state.withProperty(BlockTFMazestone.VARIANT, MazestoneVariant.CRACKED)
+		);
+		addCrumble(state -> state.getBlock() == TFBlocks.underbrick
+						&& state.getValue(BlockTFUnderBrick.VARIANT) == UnderBrickVariant.NORMAL,
+				state -> state.withProperty(BlockTFUnderBrick.VARIANT, UnderBrickVariant.CRACKED)
+		);
+		addCrumble(state -> state.getBlock() == TFBlocks.tower_wood
+						&& state.getValue(BlockTFTowerWood.VARIANT) == TowerWoodVariant.PLAIN,
+				state -> state.withProperty(BlockTFTowerWood.VARIANT, TowerWoodVariant.CRACKED)
+		);
+		addCrumble(() -> Blocks.COBBLESTONE, () -> Blocks.GRAVEL.getDefaultState());
+		addCrumble(() -> Blocks.SANDSTONE, () -> Blocks.SAND.getDefaultState());
+		addCrumble(() -> Blocks.RED_SANDSTONE,
+				() -> Blocks.SAND.getDefaultState().withProperty(BlockSand.VARIANT, BlockSand.EnumType.RED_SAND)
+		);
+		addCrumble(() -> Blocks.GRASS, () -> Blocks.DIRT.getDefaultState());
+		addCrumble(() -> Blocks.MYCELIUM, () -> Blocks.DIRT.getDefaultState());
+
+		addHarvest(() -> Blocks.GRAVEL);
+		addHarvest(() -> Blocks.DIRT);
+		addHarvest(() -> Blocks.SAND);
+	}
+
+	private void addCrumble(Supplier<Block> block, Supplier<IBlockState> result) {
+		addCrumble(state -> state.getBlock() == block.get(), state -> result.get());
+	}
+
+	private void addCrumble(Predicate<IBlockState> test, UnaryOperator<IBlockState> transform) {
+		crumbleTransforms.add(Pair.of(test, transform));
+	}
+
+	private void addHarvest(Supplier<Block> block) {
+		addHarvest(state -> state.getBlock() == block.get());
+	}
+
+	private void addHarvest(Predicate<IBlockState> test) {
+		harvestedStates.add(test);
 	}
 
 	@Override
@@ -47,7 +110,6 @@ public class ItemTFCrumbleHorn extends ItemTF {
 
 			if (crumbled > 0) {
 				stack.damageItem(crumbled, living);
-
 			}
 
 			living.world.playSound(null, living.posX, living.posY, living.posZ, SoundEvents.ENTITY_SHEEP_AMBIENT, living.getSoundCategory(), 1.0F, 0.8F);
@@ -65,8 +127,10 @@ public class ItemTFCrumbleHorn extends ItemTF {
 	}
 
 	private int doCrumble(World world, EntityLivingBase living) {
-		double range = 3.0D;
-		double radius = 2.0D;
+
+		final double range = 3.0D;
+		final double radius = 2.0D;
+
 		Vec3d srcVec = new Vec3d(living.posX, living.posY + living.getEyeHeight(), living.posZ);
 		Vec3d lookVec = living.getLookVec().scale(range);
 		Vec3d destVec = srcVec.add(lookVec);
@@ -85,50 +149,37 @@ public class ItemTFCrumbleHorn extends ItemTF {
 	}
 
 	private int crumbleBlock(World world, EntityLivingBase living, BlockPos pos) {
+
 		int cost = 0;
 
 		IBlockState state = world.getBlockState(pos);
 		Block block = state.getBlock();
 
 		if (!block.isAir(state, world, pos)) {
-			if (block == Blocks.STONE && world.rand.nextInt(CHANCE_CRUMBLE) == 0) {
-				world.setBlockState(pos, Blocks.COBBLESTONE.getDefaultState(), 3);
-				world.playEvent(2001, pos, Block.getStateId(state));
-				cost++;
-			}
 
-			if (block == Blocks.STONEBRICK && state.getValue(BlockStoneBrick.VARIANT) == BlockStoneBrick.EnumType.DEFAULT && world.rand.nextInt(CHANCE_CRUMBLE) == 0) {
-				world.setBlockState(pos, state.withProperty(BlockStoneBrick.VARIANT, BlockStoneBrick.EnumType.CRACKED), 3);
-				world.playEvent(2001, pos, Block.getStateId(state));
-				cost++;
-			}
-
-			if (block == TFBlocks.maze_stone && state.getValue(BlockTFMazestone.VARIANT) == MazestoneVariant.BRICK && world.rand.nextInt(CHANCE_CRUMBLE) == 0) {
-				world.setBlockState(pos, TFBlocks.maze_stone.getDefaultState().withProperty(BlockTFMazestone.VARIANT, MazestoneVariant.CRACKED), 3);
-				world.playEvent(2001, pos, Block.getStateId(state));
-				cost++;
-			}
-
-			if (block == Blocks.COBBLESTONE && world.rand.nextInt(CHANCE_CRUMBLE) == 0) {
-				world.setBlockState(pos, Blocks.GRAVEL.getDefaultState(), 3);
-				world.playEvent(2001, pos, Block.getStateId(state));
-				cost++;
-			}
-
-			if (block == Blocks.GRAVEL || block == Blocks.DIRT) {
-				if (living instanceof EntityPlayer) {
-					if (world.rand.nextInt(CHANCE_HARVEST) == 0 && block.canHarvestBlock(world, pos, (EntityPlayer) living)) {
-						world.setBlockToAir(pos);
-						block.harvestBlock(world, (EntityPlayer) living, pos, state, world.getTileEntity(pos), ItemStack.EMPTY);
-						world.playEvent(2001, pos, Block.getStateId(state));
-						cost++;
-					}
-				} else if (world.rand.nextInt(CHANCE_HARVEST) == 0 && ForgeEventFactory.getMobGriefingEvent(world, living)) {
-					world.destroyBlock(pos, true);
+			for (Pair<Predicate<IBlockState>, UnaryOperator<IBlockState>> transform : crumbleTransforms) {
+				if (transform.getLeft().test(state) && world.rand.nextInt(CHANCE_CRUMBLE) == 0) {
+					world.setBlockState(pos, transform.getRight().apply(state), 3);
+					world.playEvent(2001, pos, Block.getStateId(state));
 					cost++;
 				}
 			}
 
+			for (Predicate<IBlockState> predicate : harvestedStates) {
+				if (predicate.test(state) && world.rand.nextInt(CHANCE_HARVEST) == 0) {
+					if (living instanceof EntityPlayer) {
+						if (block.canHarvestBlock(world, pos, (EntityPlayer) living)) {
+							world.setBlockToAir(pos);
+							block.harvestBlock(world, (EntityPlayer) living, pos, state, world.getTileEntity(pos), ItemStack.EMPTY);
+							world.playEvent(2001, pos, Block.getStateId(state));
+							cost++;
+						}
+					} else if (ForgeEventFactory.getMobGriefingEvent(world, living)) {
+						world.destroyBlock(pos, true);
+						cost++;
+					}
+				}
+			}
 		}
 
 		return cost;
