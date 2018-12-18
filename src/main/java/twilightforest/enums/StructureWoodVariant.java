99d812f1c06e9430a0c422c2e2f06fdf6f9ee0f1
Drullkus <ericsplanet@mac.com>
2018-12-18 12:59:41 -0800

    Cleanup of 4 am programming: Why did I put suppliers?

--- src/main/java/twilightforest/enums/StructureWoodVariant.java
+++ src/main/java/twilightforest/enums/StructureWoodVariant.java
@@ -14,31 +14,31 @@ import java.util.Locale;
 import java.util.function.Supplier;
 
 public enum StructureWoodVariant implements IStringSerializable {
-    OAK          ( () -> Blocks  .PLANKS             , () -> Blocks  .OAK_STAIRS         , () -> Blocks  .WOODEN_SLAB       , () -> Blocks  .DOUBLE_WOODEN_SLAB     , () -> Blocks  .WOODEN_BUTTON      , () -> Blocks  .OAK_FENCE         , () -> Blocks  .OAK_FENCE_GATE     , () -> Blocks  .WOODEN_PRESSURE_PLATE ),
-    SPRUCE       ( () -> Blocks  .PLANKS             , () -> Blocks  .SPRUCE_STAIRS      , () -> Blocks  .WOODEN_SLAB       , () -> Blocks  .DOUBLE_WOODEN_SLAB     , () -> Blocks  .WOODEN_BUTTON      , () -> Blocks  .SPRUCE_FENCE      , () -> Blocks  .SPRUCE_FENCE_GATE  , () -> Blocks  .WOODEN_PRESSURE_PLATE ),
-    BIRCH        ( () -> Blocks  .PLANKS             , () -> Blocks  .BIRCH_STAIRS       , () -> Blocks  .WOODEN_SLAB       , () -> Blocks  .DOUBLE_WOODEN_SLAB     , () -> Blocks  .WOODEN_BUTTON      , () -> Blocks  .BIRCH_FENCE       , () -> Blocks  .BIRCH_FENCE_GATE   , () -> Blocks  .WOODEN_PRESSURE_PLATE ),
-    JUNGLE       ( () -> Blocks  .PLANKS             , () -> Blocks  .JUNGLE_STAIRS      , () -> Blocks  .WOODEN_SLAB       , () -> Blocks  .DOUBLE_WOODEN_SLAB     , () -> Blocks  .WOODEN_BUTTON      , () -> Blocks  .JUNGLE_FENCE      , () -> Blocks  .JUNGLE_FENCE_GATE  , () -> Blocks  .WOODEN_PRESSURE_PLATE ),
-    ACACIA       ( () -> Blocks  .PLANKS             , () -> Blocks  .ACACIA_STAIRS      , () -> Blocks  .WOODEN_SLAB       , () -> Blocks  .DOUBLE_WOODEN_SLAB     , () -> Blocks  .WOODEN_BUTTON      , () -> Blocks  .ACACIA_FENCE      , () -> Blocks  .ACACIA_FENCE_GATE  , () -> Blocks  .WOODEN_PRESSURE_PLATE ),
-    DARK_OAK     ( () -> Blocks  .PLANKS             , () -> Blocks  .DARK_OAK_STAIRS    , () -> Blocks  .WOODEN_SLAB       , () -> Blocks  .DOUBLE_WOODEN_SLAB     , () -> Blocks  .WOODEN_BUTTON      , () -> Blocks  .DARK_OAK_FENCE    , () -> Blocks  .DARK_OAK_FENCE_GATE, () -> Blocks  .WOODEN_PRESSURE_PLATE ),
+    OAK          ( Blocks  .PLANKS             , Blocks  .OAK_STAIRS         , Blocks  .WOODEN_SLAB       , Blocks  .DOUBLE_WOODEN_SLAB     , Blocks  .WOODEN_BUTTON      , Blocks  .OAK_FENCE         , Blocks  .OAK_FENCE_GATE     , Blocks  .WOODEN_PRESSURE_PLATE ),
+    SPRUCE       ( Blocks  .PLANKS             , Blocks  .SPRUCE_STAIRS      , Blocks  .WOODEN_SLAB       , Blocks  .DOUBLE_WOODEN_SLAB     , Blocks  .WOODEN_BUTTON      , Blocks  .SPRUCE_FENCE      , Blocks  .SPRUCE_FENCE_GATE  , Blocks  .WOODEN_PRESSURE_PLATE ),
+    BIRCH        ( Blocks  .PLANKS             , Blocks  .BIRCH_STAIRS       , Blocks  .WOODEN_SLAB       , Blocks  .DOUBLE_WOODEN_SLAB     , Blocks  .WOODEN_BUTTON      , Blocks  .BIRCH_FENCE       , Blocks  .BIRCH_FENCE_GATE   , Blocks  .WOODEN_PRESSURE_PLATE ),
+    JUNGLE       ( Blocks  .PLANKS             , Blocks  .JUNGLE_STAIRS      , Blocks  .WOODEN_SLAB       , Blocks  .DOUBLE_WOODEN_SLAB     , Blocks  .WOODEN_BUTTON      , Blocks  .JUNGLE_FENCE      , Blocks  .JUNGLE_FENCE_GATE  , Blocks  .WOODEN_PRESSURE_PLATE ),
+    ACACIA       ( Blocks  .PLANKS             , Blocks  .ACACIA_STAIRS      , Blocks  .WOODEN_SLAB       , Blocks  .DOUBLE_WOODEN_SLAB     , Blocks  .WOODEN_BUTTON      , Blocks  .ACACIA_FENCE      , Blocks  .ACACIA_FENCE_GATE  , Blocks  .WOODEN_PRESSURE_PLATE ),
+    DARK_OAK     ( Blocks  .PLANKS             , Blocks  .DARK_OAK_STAIRS    , Blocks  .WOODEN_SLAB       , Blocks  .DOUBLE_WOODEN_SLAB     , Blocks  .WOODEN_BUTTON      , Blocks  .DARK_OAK_FENCE    , Blocks  .DARK_OAK_FENCE_GATE, Blocks  .WOODEN_PRESSURE_PLATE ),
 
-    TWILIGHT_OAK ( () -> TFBlocks.twilight_oak_planks, () -> TFBlocks.twilight_oak_stairs, () -> TFBlocks.twilight_oak_slab , () -> TFBlocks.twilight_oak_doubleslab, () -> TFBlocks.twilight_oak_button, () -> TFBlocks.twilight_oak_fence, () -> TFBlocks.twilight_oak_gate  , () -> TFBlocks.twilight_oak_plate    ),
-    CANOPY       ( () -> TFBlocks.canopy_planks      , () -> TFBlocks.canopy_stairs      , () -> TFBlocks.canopy_slab       , () -> TFBlocks.canopy_doubleslab      , () -> TFBlocks.canopy_button      , () -> TFBlocks.canopy_fence      , () -> TFBlocks.canopy_gate        , () -> TFBlocks.canopy_plate          ),
-    MANGROVE     ( () -> TFBlocks.mangrove_planks    , () -> TFBlocks.mangrove_stairs    , () -> TFBlocks.mangrove_slab     , () -> TFBlocks.mangrove_doubleslab    , () -> TFBlocks.mangrove_button    , () -> TFBlocks.mangrove_fence    , () -> TFBlocks.mangrove_gate      , () -> TFBlocks.mangrove_plate        ),
-    DARK         ( () -> TFBlocks.dark_planks        , () -> TFBlocks.dark_stairs        , () -> TFBlocks.dark_slab         , () -> TFBlocks.dark_doubleslab        , () -> TFBlocks.dark_button        , () -> TFBlocks.dark_fence        , () -> TFBlocks.dark_gate          , () -> TFBlocks.dark_plate            ),
-    TIME         ( () -> TFBlocks.time_planks        , () -> TFBlocks.time_stairs        , () -> TFBlocks.time_slab         , () -> TFBlocks.time_doubleslab        , () -> TFBlocks.time_button        , () -> TFBlocks.time_fence        , () -> TFBlocks.time_gate          , () -> TFBlocks.time_plate            ),
-    TRANS        ( () -> TFBlocks.trans_planks       , () -> TFBlocks.trans_stairs       , () -> TFBlocks.trans_slab        , () -> TFBlocks.trans_doubleslab       , () -> TFBlocks.trans_button       , () -> TFBlocks.trans_fence       , () -> TFBlocks.trans_gate         , () -> TFBlocks.trans_plate           ),
-    MINE         ( () -> TFBlocks.mine_planks        , () -> TFBlocks.mine_stairs        , () -> TFBlocks.mine_slab         , () -> TFBlocks.mine_doubleslab        , () -> TFBlocks.mine_button        , () -> TFBlocks.mine_fence        , () -> TFBlocks.mine_gate          , () -> TFBlocks.mine_plate            ),
-    SORT         ( () -> TFBlocks.sort_planks        , () -> TFBlocks.sort_stairs        , () -> TFBlocks.sort_slab         , () -> TFBlocks.sort_doubleslab        , () -> TFBlocks.sort_button        , () -> TFBlocks.sort_fence        , () -> TFBlocks.sort_gate          , () -> TFBlocks.sort_plate            );
+    TWILIGHT_OAK ( TFBlocks.twilight_oak_planks, TFBlocks.twilight_oak_stairs, TFBlocks.twilight_oak_slab , TFBlocks.twilight_oak_doubleslab, TFBlocks.twilight_oak_button, TFBlocks.twilight_oak_fence, TFBlocks.twilight_oak_gate  , TFBlocks.twilight_oak_plate    ),
+    CANOPY       ( TFBlocks.canopy_planks      , TFBlocks.canopy_stairs      , TFBlocks.canopy_slab       , TFBlocks.canopy_doubleslab      , TFBlocks.canopy_button      , TFBlocks.canopy_fence      , TFBlocks.canopy_gate        , TFBlocks.canopy_plate          ),
+    MANGROVE     ( TFBlocks.mangrove_planks    , TFBlocks.mangrove_stairs    , TFBlocks.mangrove_slab     , TFBlocks.mangrove_doubleslab    , TFBlocks.mangrove_button    , TFBlocks.mangrove_fence    , TFBlocks.mangrove_gate      , TFBlocks.mangrove_plate        ),
+    DARK         ( TFBlocks.dark_planks        , TFBlocks.dark_stairs        , TFBlocks.dark_slab         , TFBlocks.dark_doubleslab        , TFBlocks.dark_button        , TFBlocks.dark_fence        , TFBlocks.dark_gate          , TFBlocks.dark_plate            ),
+    TIME         ( TFBlocks.time_planks        , TFBlocks.time_stairs        , TFBlocks.time_slab         , TFBlocks.time_doubleslab        , TFBlocks.time_button        , TFBlocks.time_fence        , TFBlocks.time_gate          , TFBlocks.time_plate            ),
+    TRANS        ( TFBlocks.trans_planks       , TFBlocks.trans_stairs       , TFBlocks.trans_slab        , TFBlocks.trans_doubleslab       , TFBlocks.trans_button       , TFBlocks.trans_fence       , TFBlocks.trans_gate         , TFBlocks.trans_plate           ),
+    MINE         ( TFBlocks.mine_planks        , TFBlocks.mine_stairs        , TFBlocks.mine_slab         , TFBlocks.mine_doubleslab        , TFBlocks.mine_button        , TFBlocks.mine_fence        , TFBlocks.mine_gate          , TFBlocks.mine_plate            ),
+    SORT         ( TFBlocks.sort_planks        , TFBlocks.sort_stairs        , TFBlocks.sort_slab         , TFBlocks.sort_doubleslab        , TFBlocks.sort_button        , TFBlocks.sort_fence        , TFBlocks.sort_gate          , TFBlocks.sort_plate            );
 
     StructureWoodVariant(
-            Supplier<Block> planks    ,
-            Supplier<Block> stairs    ,
-            Supplier<Block> slab      ,
-            Supplier<Block> doubleSlab,
-            Supplier<Block> button    ,
-            Supplier<Block> fence     ,
-            Supplier<Block> gate      ,
-            Supplier<Block> plate
+            Block planks    ,
+            Block stairs    ,
+            Block slab      ,
+            Block doubleSlab,
+            Block button    ,
+            Block fence     ,
+            Block gate      ,
+            Block plate
     ) {
         this.planks     = planks    ;
         this.stairs     = stairs    ;
@@ -55,14 +55,14 @@ public enum StructureWoodVariant implements IStringSerializable {
         return name().toLowerCase(Locale.ROOT);
     }
 
-    private final Supplier<Block> planks    ;
-    private final Supplier<Block> stairs    ;
-    private final Supplier<Block> slab      ;
-    private final Supplier<Block> doubleSlab;
-    private final Supplier<Block> button    ;
-    private final Supplier<Block> fence     ;
-    private final Supplier<Block> gate      ;
-    private final Supplier<Block> plate     ;
+    private final Block planks    ;
+    private final Block stairs    ;
+    private final Block slab      ;
+    private final Block doubleSlab;
+    private final Block button    ;
+    private final Block fence     ;
+    private final Block gate      ;
+    private final Block plate     ;
 
     @Nullable
     public static BlockPlanks.EnumType getTypeFromBlockState(IBlockState stateIn) {
@@ -156,64 +156,64 @@ public enum StructureWoodVariant implements IStringSerializable {
             case BLOCK:
                 switch (target) {
                     case OAK:
-                        return target.planks.get().getDefaultState().withProperty(BlockPlanks.VARIANT, BlockPlanks.EnumType.OAK);
+                        return target.planks.getDefaultState().withProperty(BlockPlanks.VARIANT, BlockPlanks.EnumType.OAK);
                     case SPRUCE:
-                        return target.planks.get().getDefaultState().withProperty(BlockPlanks.VARIANT, BlockPlanks.EnumType.SPRUCE);
+                        return target.planks.getDefaultState().withProperty(BlockPlanks.VARIANT, BlockPlanks.EnumType.SPRUCE);
                     case BIRCH:
-                        return target.planks.get().getDefaultState().withProperty(BlockPlanks.VARIANT, BlockPlanks.EnumType.BIRCH);
+                        return target.planks.getDefaultState().withProperty(BlockPlanks.VARIANT, BlockPlanks.EnumType.BIRCH);
                     case JUNGLE:
-                        return target.planks.get().getDefaultState().withProperty(BlockPlanks.VARIANT, BlockPlanks.EnumType.JUNGLE);
+                        return target.planks.getDefaultState().withProperty(BlockPlanks.VARIANT, BlockPlanks.EnumType.JUNGLE);
                     case ACACIA:
-                        return target.planks.get().getDefaultState().withProperty(BlockPlanks.VARIANT, BlockPlanks.EnumType.ACACIA);
+                        return target.planks.getDefaultState().withProperty(BlockPlanks.VARIANT, BlockPlanks.EnumType.ACACIA);
                     case DARK_OAK:
-                        return target.planks.get().getDefaultState().withProperty(BlockPlanks.VARIANT, BlockPlanks.EnumType.DARK_OAK);
+                        return target.planks.getDefaultState().withProperty(BlockPlanks.VARIANT, BlockPlanks.EnumType.DARK_OAK);
                     default:
-                        return target.planks.get().getDefaultState();
+                        return target.planks.getDefaultState();
                 }
             case STAIRS:
-                return transferStateKeys(stateIn, target.stairs.get().getDefaultState(), BlockStairs.FACING, BlockStairs.HALF, BlockStairs.SHAPE);
+                return transferStateKeys(stateIn, target.stairs.getDefaultState(), BlockStairs.FACING, BlockStairs.HALF, BlockStairs.SHAPE);
             case SLAB:
                 switch (target) {
                     case OAK:
-                        return transferStateKey(stateIn, target.slab.get().getDefaultState(), BlockSlab.HALF).withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.OAK);
+                        return transferStateKey(stateIn, target.slab.getDefaultState(), BlockSlab.HALF).withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.OAK);
                     case SPRUCE:
-                        return transferStateKey(stateIn, target.slab.get().getDefaultState(), BlockSlab.HALF).withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.SPRUCE);
+                        return transferStateKey(stateIn, target.slab.getDefaultState(), BlockSlab.HALF).withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.SPRUCE);
                     case BIRCH:
-                        return transferStateKey(stateIn, target.slab.get().getDefaultState(), BlockSlab.HALF).withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.BIRCH);
+                        return transferStateKey(stateIn, target.slab.getDefaultState(), BlockSlab.HALF).withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.BIRCH);
                     case JUNGLE:
-                        return transferStateKey(stateIn, target.slab.get().getDefaultState(), BlockSlab.HALF).withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.JUNGLE);
+                        return transferStateKey(stateIn, target.slab.getDefaultState(), BlockSlab.HALF).withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.JUNGLE);
                     case ACACIA:
-                        return transferStateKey(stateIn, target.slab.get().getDefaultState(), BlockSlab.HALF).withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.ACACIA);
+                        return transferStateKey(stateIn, target.slab.getDefaultState(), BlockSlab.HALF).withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.ACACIA);
                     case DARK_OAK:
-                        return transferStateKey(stateIn, target.slab.get().getDefaultState(), BlockSlab.HALF).withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.DARK_OAK);
+                        return transferStateKey(stateIn, target.slab.getDefaultState(), BlockSlab.HALF).withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.DARK_OAK);
                     default:
-                        return transferStateKey(stateIn, target.slab.get().getDefaultState(), BlockSlab.HALF);
+                        return transferStateKey(stateIn, target.slab.getDefaultState(), BlockSlab.HALF);
                 }
             case DOUBLESLAB:
                 switch (target) {
                     case OAK:
-                        return target.doubleSlab.get().getDefaultState().withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.OAK);
+                        return target.doubleSlab.getDefaultState().withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.OAK);
                     case SPRUCE:
-                        return target.doubleSlab.get().getDefaultState().withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.SPRUCE);
+                        return target.doubleSlab.getDefaultState().withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.SPRUCE);
                     case BIRCH:
-                        return target.doubleSlab.get().getDefaultState().withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.BIRCH);
+                        return target.doubleSlab.getDefaultState().withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.BIRCH);
                     case JUNGLE:
-                        return target.doubleSlab.get().getDefaultState().withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.JUNGLE);
+                        return target.doubleSlab.getDefaultState().withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.JUNGLE);
                     case ACACIA:
-                        return target.doubleSlab.get().getDefaultState().withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.ACACIA);
+                        return target.doubleSlab.getDefaultState().withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.ACACIA);
                     case DARK_OAK:
-                        return target.doubleSlab.get().getDefaultState().withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.DARK_OAK);
+                        return target.doubleSlab.getDefaultState().withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.DARK_OAK);
                     default:
-                        return target.doubleSlab.get().getDefaultState();
+                        return target.doubleSlab.getDefaultState();
                 }
             case FENCE:
-                return transferStateKeys(stateIn, target.fence.get().getDefaultState(), BlockFence.NORTH, BlockFence.EAST, BlockFence.WEST, BlockFence.SOUTH);
+                return transferStateKeys(stateIn, target.fence .getDefaultState(), BlockFence.NORTH, BlockFence.EAST, BlockFence.WEST, BlockFence.SOUTH);
             case GATE:
-                return transferStateKeys(stateIn, target.gate.get().getDefaultState(), BlockFenceGate.FACING, BlockFenceGate.OPEN, BlockFenceGate.POWERED, BlockFenceGate.IN_WALL);
+                return transferStateKeys(stateIn, target.gate  .getDefaultState(), BlockFenceGate.FACING, BlockFenceGate.OPEN, BlockFenceGate.POWERED, BlockFenceGate.IN_WALL);
             case BUTTON:
-                return transferStateKeys(stateIn, target.button.get().getDefaultState(), BlockButton.FACING, BlockButton.POWERED);
+                return transferStateKeys(stateIn, target.button.getDefaultState(), BlockButton.FACING, BlockButton.POWERED);
             case PLATE:
-                return transferStateKey(stateIn, target.plate.get().getDefaultState(), BlockPressurePlate.POWERED);
+                return transferStateKey (stateIn, target.plate .getDefaultState(), BlockPressurePlate.POWERED);
             default:
                 return stateIn; // Can't deal with this.
         }
