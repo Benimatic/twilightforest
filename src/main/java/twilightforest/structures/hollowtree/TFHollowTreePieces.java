a436b1879e178a703ab59134656e519c44263a36
Tamaized <ryanolt123@gmail.com>
2019-12-03 18:31:18 -0500

    absolute bruh moment

--- src/main/java/twilightforest/structures/hollowtree/TFHollowTreePieces.java
+++ src/main/java/twilightforest/structures/hollowtree/TFHollowTreePieces.java
@@ -5,14 +5,13 @@ import net.minecraft.world.gen.structure.MapGenStructureIO;
 public class TFHollowTreePieces {
 
 	public static void registerPieces() {
-		MapGenStructureIO.registerStructure(StructureTFHollowTreeStart.class, "TFHollowTree");
+		MapGenStructureIO.registerStructure(StructureTFHollowTreeStart.class, "TFHTLSt");
 
 		MapGenStructureIO.registerStructureComponent(ComponentTFHollowTreeLargeBranch.class, "TFHTLB");
 		MapGenStructureIO.registerStructureComponent(ComponentTFHollowTreeMedBranch.class, "TFHTMB");
 		MapGenStructureIO.registerStructureComponent(ComponentTFHollowTreeSmallBranch.class, "TFHTSB");
 		MapGenStructureIO.registerStructureComponent(ComponentTFHollowTreeTrunk.class, "TFHTTr");
 		MapGenStructureIO.registerStructureComponent(ComponentTFHollowTreeRoot.class, "TFHTRo");
-		MapGenStructureIO.registerStructure(StructureTFHollowTreeStart.class, "TFHTLSt");
 		MapGenStructureIO.registerStructureComponent(ComponentTFHollowTreeLeafDungeon.class, "TFHTLD");
 
 	}
