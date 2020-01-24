5865ea144e89055bbc5fe00aa3b7fc7ae12f699d
Andromander <cathy@feodoroff.com>
2020-01-24 11:04:48 +1000

    Actually fix Transformation Powder

--- src/main/java/twilightforest/item/ItemTFTransformPowder.java
+++ src/main/java/twilightforest/item/ItemTFTransformPowder.java
@@ -1,8 +1,6 @@
 package twilightforest.item;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.*;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.item.ItemStack;
@@ -11,15 +9,13 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.particles.ParticleTypes;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 import twilightforest.TwilightForestMod;
-import twilightforest.util.TFEntityNames;
-import twilightforest.util.VanillaEntityNames;
+import twilightforest.entity.TFEntities;
 
 import javax.annotation.Nonnull;
 import java.util.HashMap;
@@ -28,29 +24,29 @@ import java.util.UUID;
 
 public class ItemTFTransformPowder extends ItemTF {
 
-	private final Map<ResourceLocation, ResourceLocation> transformMap = new HashMap<>();
+	private final Map<EntityType<?>, EntityType<?>> transformMap = new HashMap<>();
 
 	protected ItemTFTransformPowder(Properties props) {
 		super(props);
 		//this.maxStackSize = 64; TODO: Is this needed?
 
-		addTwoWayTransformation(TFEntityNames.MINOTAUR,       VanillaEntityNames.ZOMBIE_PIGMAN);
-		addTwoWayTransformation(TFEntityNames.DEER,           VanillaEntityNames.COW);
-		addTwoWayTransformation(TFEntityNames.BIGHORN_SHEEP,  VanillaEntityNames.SHEEP);
-		addTwoWayTransformation(TFEntityNames.WILD_BOAR,      VanillaEntityNames.PIG);
-		addTwoWayTransformation(TFEntityNames.BUNNY,          VanillaEntityNames.RABBIT);
-		addTwoWayTransformation(TFEntityNames.TINY_BIRD,      VanillaEntityNames.PARROT);
-		addTwoWayTransformation(TFEntityNames.RAVEN,          VanillaEntityNames.BAT);
-		addTwoWayTransformation(TFEntityNames.HOSTILE_WOLF,   VanillaEntityNames.WOLF);
-		addTwoWayTransformation(TFEntityNames.PENGUIN,        VanillaEntityNames.CHICKEN);
-		addTwoWayTransformation(TFEntityNames.HEDGE_SPIDER,   VanillaEntityNames.SPIDER);
-		addTwoWayTransformation(TFEntityNames.SWARM_SPIDER,   VanillaEntityNames.CAVE_SPIDER);
-		addTwoWayTransformation(TFEntityNames.WRAITH,         VanillaEntityNames.BLAZE);
-		addTwoWayTransformation(TFEntityNames.REDCAP,         VanillaEntityNames.VILLAGER);
-		addTwoWayTransformation(TFEntityNames.SKELETON_DRUID, VanillaEntityNames.WITCH);
+		addTwoWayTransformation(TFEntities.minotaur.get(),       EntityType.ZOMBIE_PIGMAN);
+		addTwoWayTransformation(TFEntities.deer.get(),           EntityType.COW);
+		addTwoWayTransformation(TFEntities.bighorn_sheep.get(),  EntityType.SHEEP);
+		addTwoWayTransformation(TFEntities.wild_boar.get(),      EntityType.PIG);
+		addTwoWayTransformation(TFEntities.bunny.get(),          EntityType.RABBIT);
+		addTwoWayTransformation(TFEntities.tiny_bird.get(),      EntityType.PARROT);
+		addTwoWayTransformation(TFEntities.raven.get(),          EntityType.BAT);
+		addTwoWayTransformation(TFEntities.hostile_wolf.get(),   EntityType.WOLF);
+		addTwoWayTransformation(TFEntities.penguin.get(),        EntityType.CHICKEN);
+		addTwoWayTransformation(TFEntities.hedge_spider.get(),   EntityType.SPIDER);
+		addTwoWayTransformation(TFEntities.swarm_spider.get(),   EntityType.CAVE_SPIDER);
+		addTwoWayTransformation(TFEntities.wraith.get(),         EntityType.BLAZE);
+		addTwoWayTransformation(TFEntities.redcap.get(),         EntityType.VILLAGER);
+		addTwoWayTransformation(TFEntities.skeleton_druid.get(), EntityType.WITCH);
 	}
 
-	private void addTwoWayTransformation(ResourceLocation from, ResourceLocation to) {
+	private void addTwoWayTransformation(EntityType<?> from, EntityType<?> to) {
 		transformMap.put(from, to);
 		transformMap.put(to, from);
 	}
@@ -60,24 +56,20 @@ public class ItemTFTransformPowder extends ItemTF {
 
 		if (!target.isAlive()) return false;
 
-		ResourceLocation location = transformMap.get(EntityType.getKey(target.getType()));
-		if (location == null) return false;
+//		ResourceLocation location = transformMap.get(EntityType.getKey(target.getType()));
+//		if (location == null) return false;
 
-		if (target.world.isRemote) {
-			return EntityList.isRegistered(location);
-		}
-
-		Entity newEntity = EntityList.createEntityByIDFromName(location, target.world);
+		Entity newEntity = transformMap.get(target.getType()).create(target.world);
 		if (newEntity == null) return false;
 
 		newEntity.setLocationAndAngles(target.getX(), target.getY(), target.getZ(), target.rotationYaw, target.rotationPitch);
-		if (newEntity instanceof LivingEntity) {
-			((LivingEntity) newEntity).onInitialSpawn(target.world.getDifficultyForLocation(new BlockPos(target)), null);
+		if (newEntity instanceof MobEntity) {
+			((MobEntity) newEntity).onInitialSpawn(target.world, target.world.getDifficultyForLocation(new BlockPos(target)), SpawnReason.CONVERSION, null, null);
 		}
 
 		try { // try copying what can be copied
 			UUID uuid = newEntity.getUniqueID();
-			newEntity.read(target.writeToNBT(newEntity.writeToNBT(new CompoundNBT())));
+			newEntity.read(target.writeWithoutTypeId(newEntity.writeWithoutTypeId(new CompoundNBT())));
 			newEntity.setUniqueId(uuid);
 		} catch (Exception e) {
 			TwilightForestMod.LOGGER.warn("Couldn't transform entity NBT data: {}", e);
@@ -87,9 +79,9 @@ public class ItemTFTransformPowder extends ItemTF {
 		target.remove();
 		stack.shrink(1);
 
-		if (target instanceof LivingEntity) {
-			((LivingEntity) target).spawnExplosionParticle();
-			((LivingEntity) target).spawnExplosionParticle();
+		if (target instanceof MobEntity) {
+			((MobEntity) target).spawnExplosionParticle();
+			((MobEntity) target).spawnExplosionParticle();
 		}
 		target.playSound(SoundEvents.ENTITY_ZOMBIE_VILLAGER_CURE, 1.0F + random.nextFloat(), random.nextFloat() * 0.7F + 0.3F);
 
@@ -104,7 +96,7 @@ public class ItemTFTransformPowder extends ItemTF {
 
 			// particle effect
 			for (int i = 0; i < 30; i++) {
-				world.addParticle(ParticleTypes.CRIT_MAGIC, fanBox.minX + world.rand.nextFloat() * (fanBox.maxX - fanBox.minX),
+				world.addParticle(ParticleTypes.CRIT, fanBox.minX + world.rand.nextFloat() * (fanBox.maxX - fanBox.minX),
 						fanBox.minY + world.rand.nextFloat() * (fanBox.maxY - fanBox.minY),
 						fanBox.minZ + world.rand.nextFloat() * (fanBox.maxZ - fanBox.minZ),
 						0, 0, 0);
