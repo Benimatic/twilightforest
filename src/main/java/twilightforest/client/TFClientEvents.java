0f4b7b916adde1b7e335c0dad678a56011ea3007
Vincent Lee <vincent.lee@outlook.com>
2020-06-09 08:51:06 -0500

    Fix 3rd person shield renderer

--- src/main/java/twilightforest/client/TFClientEvents.java
+++ src/main/java/twilightforest/client/TFClientEvents.java
@@ -28,11 +28,13 @@ import twilightforest.world.TFGenerationSettings;
 @Mod.EventBusSubscriber(modid = TwilightForestMod.ID, value = Dist.CLIENT)
 public class TFClientEvents {
 
-	@SubscribeEvent
-	public static void texStitch(TextureStitchEvent.Pre evt) {
-		AtlasTexture map = evt.getMap();
+	@Mod.EventBusSubscriber(modid = TwilightForestMod.ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+	public static class ModBusEvents {
+		@SubscribeEvent
+		public static void texStitch(TextureStitchEvent.Pre evt) {
+			AtlasTexture map = evt.getMap();
 
-        //TODO: Removed until Tinkers' Construct is available
+			//TODO: Removed until Tinkers' Construct is available
 		/*map.setTextureEntry( new MoltenFieryTexture   ( new ResourceLocation( "minecraft", "blocks/lava_still"  ), RegisterBlockEvent.moltenFieryStill                                        ));
 		map.setTextureEntry( new MoltenFieryTexture   ( new ResourceLocation( "minecraft", "blocks/lava_flow"   ), RegisterBlockEvent.moltenFieryFlow                                         ));
 		map.setTextureEntry( new GradientMappedTexture( new ResourceLocation( "minecraft", "blocks/lava_still"  ), RegisterBlockEvent.moltenKnightmetalStill, true, KNIGHTMETAL_GRADIENT_MAP  ));
@@ -40,7 +42,7 @@ public class TFClientEvents {
 		map.setTextureEntry( new GradientMappedTexture( new ResourceLocation( "minecraft", "blocks/water_still" ), RegisterBlockEvent.essenceFieryStill     , true, FIERY_ESSENCE_GRADIENT_MAP));
 		map.setTextureEntry( new GradientMappedTexture( new ResourceLocation( "minecraft", "blocks/water_flow"  ), RegisterBlockEvent.essenceFieryFlow      , true, FIERY_ESSENCE_GRADIENT_MAP));*/
 
-		//TODO: Removed until Immersive Engineering is available
+			//TODO: Removed until Immersive Engineering is available
 		/*if (TFCompat.IMMERSIVEENGINEERING.isActivated()) {
 			map.setTextureEntry( new GradientMappedTexture( new ResourceLocation( "immersiveengineering", "revolvers/shaders/revolver_grip" ), IEShaderRegister.PROCESSED_REVOLVER_GRIP_LAYER, true, EASY_GRAYSCALING_MAP ));
 			map.setTextureEntry( new GradientMappedTexture( new ResourceLocation( "immersiveengineering", "revolvers/shaders/revolver_0"    ), IEShaderRegister.PROCESSED_REVOLVER_LAYER     , true, EASY_GRAYSCALING_MAP ));
@@ -63,9 +65,9 @@ public class TFClientEvents {
 				}
 			}
 		}*/
-	}
+		}
 
-	//TODO: Fields are unused due to missing compat
+		//TODO: Fields are unused due to missing compat
 	/*public static final GradientNode[] KNIGHTMETAL_GRADIENT_MAP = {
 			new GradientNode(0.0f , 0xFF_33_32_32),
 			new GradientNode(0.1f , 0xFF_6A_73_5E),
@@ -86,33 +88,23 @@ public class TFClientEvents {
 			new GradientNode(1.0f, 0xFF_FF_FF_FF)
 	};*/
 
-	@SubscribeEvent
-	public static void registerModels(ModelRegistryEvent event) {
-		ModelLoader.addSpecialModel(LayerShields.LOC);
+		@SubscribeEvent
+		public static void registerModels(ModelRegistryEvent event) {
+			ModelLoader.addSpecialModel(LayerShields.LOC);
+		}
 	}
 
 	/**
 	 * Stop the game from rendering the mount health for unfriendly creatures
 	 */
 	@SubscribeEvent
-	public static boolean preOverlay(RenderGameOverlayEvent.Pre event) {
+	public static void preOverlay(RenderGameOverlayEvent.Pre event) {
 		if (event.getType() == RenderGameOverlayEvent.ElementType.HEALTHMOUNT) {
 			if (TFEventListener.isRidingUnfriendly(Minecraft.getInstance().player)) {
 				event.setCanceled(true);
-				return false;
 			}
 		}
-		return true;
 	}
-
-	//FIXME shove onto an external player layer, like armor
-	//@SubscribeEvent
-	//public static void renderLivingPre(RenderLivingEvent.Pre<LivingEntity> event) {
-	//	// Shields
-	//	if (event.getEntity().hasCapability(CapabilityList.SHIELDS, null)) {
-	//		ShaderManager.useShader(ShaderManager.shieldShader, ShaderManager.TIME);
-	//	}
-	//}
 
 	/**
 	 * Render effects in first-person perspective
--- src/main/java/twilightforest/client/renderer/entity/LayerShields.java
+++ src/main/java/twilightforest/client/renderer/entity/LayerShields.java
@@ -1,19 +1,17 @@
 package twilightforest.client.renderer.entity;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.Atlases;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.Vector3f;
 import net.minecraft.client.renderer.entity.IEntityRenderer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.client.renderer.model.ModelResourceLocation;
-import net.minecraft.client.renderer.texture.AtlasTexture;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.inventory.container.PlayerContainer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
@@ -38,81 +36,58 @@ public class LayerShields<T extends LivingEntity, M extends EntityModel<T>> exte
 	}
 
 	private int getShieldCount(T entity) {
-		EntityTFLich lich = null;
-		if (entity instanceof EntityTFLich)
-			lich = (EntityTFLich) entity;
-
-		int capShields = entity.getCapability(CapabilityList.SHIELDS).map(IShieldCapability::shieldsLeft).orElse(0);
-
-		return lich != null ? lich.getShieldStrength() : capShields;
+		return entity instanceof EntityTFLich
+						? ((EntityTFLich) entity).getShieldStrength()
+						: entity.getCapability(CapabilityList.SHIELDS).map(IShieldCapability::shieldsLeft).orElse(0);
 	}
 
-	private void renderShields(MatrixStack stack, IRenderTypeBuffer buffer, int light, float scale, T entity, float partialTicks) {
-		float rotateAngleY = (entity.ticksExisted + partialTicks) / 5.0F;
-		float rotateAngleX = MathHelper.sin((entity.ticksExisted + partialTicks) / 5.0F) / 4.0F;
-		float rotateAngleZ = MathHelper.cos((entity.ticksExisted + partialTicks) / 5.0F) / 4.0F;
-
-		// float prevX = OpenGlHelper.lastBrightnessX, prevY = OpenGlHelper.lastBrightnessY;
-		// OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240f, 240f);
-		// GL11.glMaterial(GL11.GL_FRONT_AND_BACK, GL11.GL_EMISSION, RenderHelper.setColorBuffer(1f, 1f, 1f, 1f));
+	private void renderShields(MatrixStack stack, IRenderTypeBuffer buffer, T entity, float partialTicks) {
+		float age = entity.ticksExisted + partialTicks;
+		float rotateAngleY = age / 5.0F;
+		float rotateAngleX = MathHelper.sin(age / 5.0F) / 4.0F;
+		float rotateAngleZ = MathHelper.cos(age / 5.0F) / 4.0F;
 
 		int count = getShieldCount(entity);
 		for (int c = 0; c < count; c++) {
 			stack.push();
 
-			RenderSystem.rotatef(rotateAngleZ * (180F / (float) Math.PI), 0.0F, 0.0F, 1.0F);
-			RenderSystem.rotatef(rotateAngleY * (180F / (float) Math.PI) + (c * (360F / count)), 0.0F, 1.0F, 0.0F);
-			RenderSystem.rotatef(rotateAngleX * (180F / (float) Math.PI), 1.0F, 0.0F, 0.0F);
+			// shift to the torso
+			stack.translate(-0.5, 0, -0.5);
 
-			// It's upside-down, gotta make it upside-up
-			stack.scale(scale, -scale, scale);
+			// invert Y
+			stack.scale(1, -1, 1);
 
-			// Move the draw away from the entity being drawn around
-			stack.translate(0F, 0F, 1F);
+			// perform the rotations, accounting for the fact that baked models are corner-based
+			stack.translate(0.5, 0.5, 0.5);
+			stack.multiply(Vector3f.POSITIVE_Z.getDegreesQuaternion(rotateAngleZ * (180F / (float) Math.PI)));
+			stack.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(rotateAngleY * (180F / (float) Math.PI) + (c * (360F / count))));
+			stack.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(rotateAngleX * (180F / (float) Math.PI)));
+			stack.translate(-0.5, -0.5, -0.5);
 
-			// There has got to be a sane way to do this, if anyone knows better feel free to change this
-			// TODO: make sure this even works
+			// push the shields outwards from the center of rotation
+			stack.translate(0F, 0F, -0.7F);
+
 			IBakedModel model = Minecraft.getInstance().getModelManager().getModel(LOC);
 			for (Direction dir : DIRS) {
 				RAND.setSeed(42L);
 				Minecraft.getInstance().getItemRenderer().renderBakedItemQuads(
-
 						stack,
-
-						buffer.getBuffer(RenderType.getEnergySwirl(PlayerContainer.BLOCK_ATLAS_TEXTURE, 0, 0)),
-
+						buffer.getBuffer(Atlases.getEntityTranslucent()),
 						model.getQuads(null, dir, RAND, EmptyModelData.INSTANCE),
-
 						ItemStack.EMPTY,
-
-						light,
-
+						0xF000F0,
 						OverlayTexture.DEFAULT_UV
-
 				);
 			}
 
-
 			stack.pop();
 		}
-
-		// GL11.glMaterial(GL11.GL_FRONT_AND_BACK, GL11.GL_EMISSION, RenderHelper.setColorBuffer(0f, 0f, 0f, 1f));
-		// OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, prevX, prevY);
 	}
 
 	@Override
-	public void render(MatrixStack stack, IRenderTypeBuffer buffer, int i, T living, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+	public void render(MatrixStack stack, IRenderTypeBuffer buffer, int light, T living, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
 		if (getShieldCount(living) > 0) {
-			// GlStateManager.enableBlend();
-			// GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-			// RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0f);
-			float scale = 1F; // TODO
-			renderShields(stack, buffer, i, scale * 13, living, partialTicks);
+			renderShields(stack, buffer, living, partialTicks);
 		}
 	}
-	//
-	//    @Override
-	//    public boolean shouldCombineTextures() {
-	//        return false;
-	//    }
 }
