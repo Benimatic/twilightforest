fb4dd48a6394fbaf3e77870792b1e03c2bd5ebfb
Tamaized <9671313+Tamaized@users.noreply.github.com>
2020-07-19 18:35:18 -0400

    Bare bones dimension json. Still need to look into the seed

--- src/main/java/twilightforest/world/TFDimensions.java
+++ src/main/java/twilightforest/world/TFDimensions.java
@@ -37,22 +37,6 @@ public class TFDimensions {
 	public static final RegistryKey<DimensionType> twilight_forest = RegistryKey.func_240903_a_(Registry.DIMENSION_TYPE_KEY, new ResourceLocation("twilight_forest"));
 	public static final RegistryKey<World> twilight_forest_world = RegistryKey.func_240903_a_(Registry.WORLD_KEY, new ResourceLocation("twilight_forest"));
 
-	public static final DimensionSettings.Preset tf_preset = new DimensionSettings.Preset("twilight_forest", (preset) -> new DimensionSettings(
-			new DimensionStructuresSettings(false), new NoiseSettings(
-					256, //functional height
-					new ScalingSettings(0.9999999814507745D, 0.9999999814507745D, 80.0D, 160.0D), //sampling. 2nd and 4th are Y noise. TODO: Modify for us?
-					new SlideSettings(-10, 3, 0), //top slide
-					new SlideSettings(-30, 0, 0), //bottom slide
-					1, //size_horizontal
-					2, //size_vertical TODO: Modify?
-					1.0D, //density_factor
-					-0.46875D, //density_offset
-					true, //simplex noise
-					true, //random density offset
-					false, //island noise override
-					false //amplified TODO: Should we allow it?
-			), Blocks.STONE.getDefaultState(), Blocks.WATER.getDefaultState(), -10, 0, 31, false, Optional.of(preset)));
-
 	//TODO: Do we even need this anymore? Unless someone uses our exact mod id and registry name, this shouldn't be a problem
 //	public static void checkOriginDimension() {
 //		ResourceLocation tfDim = new ResourceLocation(TwilightForestMod.ID, "twilight_forest");
