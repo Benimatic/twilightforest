34e55bfd0093be4a68bd15c9c0566cb0c4c30478
Tamaized <9671313+Tamaized@users.noreply.github.com>
2021-02-28 18:46:25 -0500

    bring back leaf sway particle rotations are fucking retarded
    Signed-off-by: Tamaized <9671313+Tamaized@users.noreply.github.com>

--- src/main/java/twilightforest/client/particle/ParticleLeaf.java
+++ src/main/java/twilightforest/client/particle/ParticleLeaf.java
@@ -5,7 +5,9 @@ import net.minecraft.client.particle.*;
 import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.vector.Quaternion;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.math.vector.Vector3f;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -13,6 +15,7 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 public class ParticleLeaf extends SpriteTexturedParticle {
 
 	private final Vector3d target;
+	private float rot;
 
 	ParticleLeaf(ClientWorld world, double x, double y, double z, double vx, double vy, double vz) {
 		this(world, x, y, z, vx, vy, vz, 1.0F);
@@ -34,6 +37,7 @@ public class ParticleLeaf extends SpriteTexturedParticle {
 		this.maxAge = 160 + ((int) (rand.nextFloat() * 30F));
 		this.maxAge = (int) (this.maxAge * scale);
 		this.canCollide = true;
+		this.prevParticleAngle = this.particleAngle = rand.nextFloat() * 2F - 1F;
 	}
 
 	@Override
@@ -60,6 +64,7 @@ public class ParticleLeaf extends SpriteTexturedParticle {
 			this.motionX *= 0.699999988079071D;
 			this.motionZ *= 0.699999988079071D;
 		} else {
+			rot += 5F;
 			if (motionX == 0)
 				motionX += (rand.nextBoolean() ? 1 : -1) * 0.001F;
 			if (motionZ == 0)
@@ -74,7 +79,36 @@ public class ParticleLeaf extends SpriteTexturedParticle {
 	@Override
 	public void renderParticle(IVertexBuilder buffer, ActiveRenderInfo entity, float partialTicks) {
 		particleAlpha = Math.min(MathHelper.clamp(age, 0, 20) / 20F, MathHelper.clamp(maxAge - age, 0, 20) / 20F);
-		super.renderParticle(buffer, entity, partialTicks);
+		Vector3d lvt_4_1_ = entity.getProjectedView();
+		float lvt_5_1_ = (float)(MathHelper.lerp((double)partialTicks, this.prevPosX, this.posX) - lvt_4_1_.getX());
+		float lvt_6_1_ = (float)(MathHelper.lerp((double)partialTicks, this.prevPosY, this.posY) - lvt_4_1_.getY());
+		float lvt_7_1_ = (float)(MathHelper.lerp((double)partialTicks, this.prevPosZ, this.posZ) - lvt_4_1_.getZ());
+		Quaternion lvt_8_2_ = new Quaternion(entity.getRotation());
+		if (this.particleAngle != 0.0F) {
+			float lvt_9_1_ = MathHelper.lerp(partialTicks, this.prevParticleAngle, this.particleAngle);
+			lvt_8_2_.multiply(Vector3f.ZP.rotation(lvt_9_1_));
+		}
+		lvt_8_2_.multiply(Vector3f.YP.rotation(MathHelper.cos((float) Math.toRadians(rot % 360F))));
+		Vector3f lvt_9_2_ = new Vector3f(-1.0F, -1.0F, 0.0F);
+		lvt_9_2_.transform(lvt_8_2_);
+		Vector3f[] lvt_10_1_ = new Vector3f[]{new Vector3f(-1.0F, -1.0F, 0.0F), new Vector3f(-1.0F, 1.0F, 0.0F), new Vector3f(1.0F, 1.0F, 0.0F), new Vector3f(1.0F, -1.0F, 0.0F)};
+		float lvt_11_1_ = this.getScale(partialTicks);
+
+		for(int lvt_12_1_ = 0; lvt_12_1_ < 4; ++lvt_12_1_) {
+			Vector3f lvt_13_1_ = lvt_10_1_[lvt_12_1_];
+			lvt_13_1_.transform(lvt_8_2_);
+			lvt_13_1_.mul(lvt_11_1_);
+			lvt_13_1_.add(lvt_5_1_, lvt_6_1_, lvt_7_1_);
+		}
+		float lvt_12_2_ = this.getMinU();
+		float lvt_13_2_ = this.getMaxU();
+		float lvt_14_1_ = this.getMinV();
+		float lvt_15_1_ = this.getMaxV();
+		int lvt_16_1_ = this.getBrightnessForRender(partialTicks);
+		buffer.pos((double)lvt_10_1_[0].getX(), (double)lvt_10_1_[0].getY(), (double)lvt_10_1_[0].getZ()).tex(lvt_13_2_, lvt_15_1_).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(lvt_16_1_).endVertex();
+		buffer.pos((double)lvt_10_1_[1].getX(), (double)lvt_10_1_[1].getY(), (double)lvt_10_1_[1].getZ()).tex(lvt_13_2_, lvt_14_1_).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(lvt_16_1_).endVertex();
+		buffer.pos((double)lvt_10_1_[2].getX(), (double)lvt_10_1_[2].getY(), (double)lvt_10_1_[2].getZ()).tex(lvt_12_2_, lvt_14_1_).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(lvt_16_1_).endVertex();
+		buffer.pos((double)lvt_10_1_[3].getX(), (double)lvt_10_1_[3].getY(), (double)lvt_10_1_[3].getZ()).tex(lvt_12_2_, lvt_15_1_).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(lvt_16_1_).endVertex();
 	}
 
 	@Override
