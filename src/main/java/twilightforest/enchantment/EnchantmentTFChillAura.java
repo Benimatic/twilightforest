f90421bcaeb2f1db6231ff91d05390d1acccab0e
GizmoTheMoonPig <cnicholasb@comcast.net>
2021-05-01 13:30:38 -0600

    prevent our enchants from showing up during enchanting, closes #1111

--- src/main/java/twilightforest/enchantment/EnchantmentTFChillAura.java
+++ src/main/java/twilightforest/enchantment/EnchantmentTFChillAura.java
@@ -3,12 +3,18 @@ package twilightforest.enchantment;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
 
 public class EnchantmentTFChillAura extends Enchantment {
 	// TODO implement
 	public EnchantmentTFChillAura(Rarity rarity) {
 		super(rarity, EnchantmentType.ARMOR, new EquipmentSlotType[] { EquipmentSlotType.HEAD, EquipmentSlotType.CHEST,
 				EquipmentSlotType.LEGS, EquipmentSlotType.FEET });
+	}
+
+	@Override
+	public boolean canApplyAtEnchantingTable(ItemStack stack) {
+		return false;
 	}
 
 	@Override
--- src/main/java/twilightforest/enchantment/EnchantmentTFFireReact.java
+++ src/main/java/twilightforest/enchantment/EnchantmentTFFireReact.java
@@ -3,6 +3,7 @@ package twilightforest.enchantment;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
 
 public class EnchantmentTFFireReact extends Enchantment {
 	// TODO implement
@@ -11,6 +12,11 @@ public class EnchantmentTFFireReact extends Enchantment {
 				EquipmentSlotType.HEAD, EquipmentSlotType.CHEST,
 				EquipmentSlotType.LEGS, EquipmentSlotType.FEET
 		});
+	}
+
+	@Override
+	public boolean canApplyAtEnchantingTable(ItemStack stack) {
+		return false;
 	}
 	
 	@Override
