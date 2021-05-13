acb8c8559d237a4a70bbe5cba55214e419ba98cd
GizmoTheMoonPig <cnicholasb@comcast.net>
2021-05-13 09:52:49 -0600

    Fixed TF loading screen for real

--- src/main/java/twilightforest/client/LoadingScreenListener.java
+++ src/main/java/twilightforest/client/LoadingScreenListener.java
@@ -3,35 +3,27 @@ package twilightforest.client;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.DownloadTerrainScreen;
 import net.minecraft.util.RegistryKey;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import twilightforest.TwilightForestMod;
+import twilightforest.block.TFBlocks;
 import twilightforest.world.TFDimensions;
 
 @OnlyIn(Dist.CLIENT)
 public class LoadingScreenListener {
 
 	private final Minecraft client = Minecraft.getInstance();
-	private RegistryKey<World> lastDimension = World.OVERWORLD; //overworld
-
-	@SubscribeEvent
-	public void onPlayerTick(TickEvent.PlayerTickEvent event) {
-		if (event.phase == TickEvent.Phase.END && event.player == client.player) {
-			lastDimension = event.player.getEntityWorld().getDimensionKey();
-		}
-	}
 
 	@SubscribeEvent
 	public void onOpenGui(GuiOpenEvent event) {
 		if (event.getGui() instanceof DownloadTerrainScreen && client.player != null) {
-			if (client.player.getEntityWorld().getDimensionKey() == TFDimensions.twilightForest || lastDimension == TFDimensions.twilightForest) {
+			if (client.player.getEntityWorld().getBlockState(client.player.getPosition().down()) == TFBlocks.twilight_portal.get().getDefaultState() || client.player.getEntityWorld().getDimensionKey() == TFDimensions.twilightForest) {
 				GuiTwilightForestLoading guiLoading = new GuiTwilightForestLoading();
-				guiLoading.setEntering(client.player.getEntityWorld().getDimensionKey() == TFDimensions.twilightForest);
+				guiLoading.setEntering(client.player.getEntityWorld().getDimensionKey() == World.OVERWORLD);
 				event.setGui(guiLoading);
 			}
 		}
