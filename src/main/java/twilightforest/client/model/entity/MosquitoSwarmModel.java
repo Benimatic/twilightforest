e751ca14b819f969957e37e35a2d5e48dc6a14ff
Drullkus <failbookftw@gmail.com>
2021-07-31 19:45:40 -0700

    Two more ports for Mosquito Swarm and Snow Queen
    Pushing because I gotta detour off of this for a moment

--- src/main/java/twilightforest/client/model/entity/MosquitoSwarmModel.java
+++ src/main/java/twilightforest/client/model/entity/MosquitoSwarmModel.java
@@ -1,129 +1,118 @@
 package twilightforest.client.model.entity;
 
-import com.google.common.collect.ImmutableList;
-import net.minecraft.client.model.ListModel;
+import net.minecraft.client.model.HierarchicalModel;
 import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.model.geom.builders.MeshDefinition;
+import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.core.Direction;
 import net.minecraft.util.Mth;
 import net.minecraft.world.phys.Vec3;
 import twilightforest.entity.MosquitoSwarmEntity;
 
 import java.util.Random;
 
-public class MosquitoSwarmModel extends ListModel<MosquitoSwarmEntity> {
+public class MosquitoSwarmModel extends HierarchicalModel<MosquitoSwarmEntity> {
+	private final ModelPart root, core, group1, group2, group3, group4, group5, group6;
 
-	ModelPart core;
-	ModelPart node1;
-	ModelPart node2;
-	ModelPart node3;
-	ModelPart node4;
-	ModelPart node5;
-	ModelPart node6;
+	private static final Random rand = new Random();
 
-	Random rand = new Random();
+	public MosquitoSwarmModel(ModelPart root) {
+		this.root = root;
 
-	public MosquitoSwarmModel() {
-		core = new ModelPart(this, rand.nextInt(28), rand.nextInt(28));
-		core.addBox(-0.5F, 2F, -0.5F, 1, 1, 1);
-		core.setPos(0.0F, -4.0F, 0.0F);
+		this.core = this.root.getChild("core");
 
-		node1 = new ModelPart(this, rand.nextInt(28), rand.nextInt(28));
-		node1.addBox(0F, 0F, -11F, 1, 1, 1);
-		node1.setPos(-0.5F, -2F, -0.5F);
-		core.addChild(node1);
-
-		node2 = new ModelPart(this, rand.nextInt(28), rand.nextInt(28));
-		node2.addBox(0F, -11F, 0F, 1, 1, 1);
-		node2.setPos(-0.5F, -2F, -0.5F);
-		core.addChild(node2);
-
-		node3 = new ModelPart(this, rand.nextInt(28), rand.nextInt(28));
-		node3.addBox(-11F, 0F, 0F, 1, 1, 1);
-		node3.setPos(-0.5F, -2F, -0.5F);
-		core.addChild(node3);
-
-		node4 = new ModelPart(this, rand.nextInt(28), rand.nextInt(28));
-		node4.addBox(0F, 0F, 11F, 1, 1, 1);
-		node4.setPos(-0.5F, -2F, -0.5F);
-		core.addChild(node4);
-
-		node5 = new ModelPart(this, rand.nextInt(28), rand.nextInt(28));
-		node5.addBox(0F, 11F, 0F, 1, 1, 1);
-		node5.setPos(-0.5F, -2F, -0.5F);
-		core.addChild(node5);
-
-		node6 = new ModelPart(this, rand.nextInt(28), rand.nextInt(28));
-		node6.addBox(11F, 0F, 0F, 1, 1, 1);
-		node6.setPos(-0.5F, -2F, -0.5F);
-		core.addChild(node6);
-
-		addBugsToNodes(node1);
-		addBugsToNodes(node2);
-		addBugsToNodes(node3);
-		addBugsToNodes(node4);
-		addBugsToNodes(node5);
-		addBugsToNodes(node6);
+		this.group1 = this.root.getChild("group_1");
+		this.group2 = this.root.getChild("group_2");
+		this.group3 = this.root.getChild("group_3");
+		this.group4 = this.root.getChild("group_4");
+		this.group5 = this.root.getChild("group_5");
+		this.group6 = this.root.getChild("group_6");
 	}
 
-	@Override
-	public Iterable<ModelPart> parts() {
-		return ImmutableList.of(core, node1, node2, node3, node4, node5, node6);
+	public static LayerDefinition create() {
+		MeshDefinition mesh = new MeshDefinition();
+		PartDefinition partRoot = mesh.getRoot();
+
+		var core = partRoot.addOrReplaceChild("core", CubeListBuilder.create()
+						.texOffs(rand.nextInt(28), rand.nextInt(28))
+						.addBox(-0.5F, 2F, -0.5F, 1, 1, 1),
+				PartPose.offset(0.0F, -4.0F, 0.0F));
+
+		PartPose offset = PartPose.offset(-0.5F, -2F, -0.5F);
+
+		for (Direction dir : Direction.values()) {
+			addBugsToGroup(dir.ordinal(), core.addOrReplaceChild("group_" + dir.ordinal(), CubeListBuilder.create()
+							.texOffs(rand.nextInt(28), rand.nextInt(28))
+							.addBox(dir.getStepX() * 11, dir.getStepY() * 11, dir.getStepZ() * 11, 1, 1, 1),
+					offset));
+		}
+
+		return LayerDefinition.create(mesh, 64, 64);
 	}
 
 	/**
-	 * Add the proper number of mosquitoes to the nodes
+	 * Add the proper number of mosquitoes to the groups
 	 */
-	public void addBugsToNodes(ModelPart node) {
-		int bugs = 16;
+	public static void addBugsToGroup(int iteration, PartDefinition parent) {
+		final int bugs = 16;
 
 		for (int i = 0; i < bugs; i++) {
 			Vec3 vec = new Vec3(0, 0, 0);
-			float rotateY = ((i * (360F / bugs)) * 3.141593F) / 180F;
+			float rotateY = ((i * (360F / bugs)) * Mth.PI) / 180F;
 			vec.xRot(rotateY);
-			ModelPart bug = new ModelPart(this, rand.nextInt(28), rand.nextInt(28));
 
 			float bugX = (rand.nextFloat() - rand.nextFloat()) * 16.0f;
 			float bugY = (rand.nextFloat() - rand.nextFloat()) * 16.0f;
 			float bugZ = (rand.nextFloat() - rand.nextFloat()) * 16.0f;
 
-			bug.addBox(bugX, bugY, bugZ, 1, 1, 1);
-
-			bug.setPos((float) vec.x, (float) vec.y, (float) vec.z);
-			bug.yRot = rotateY;
-			node.addChild(bug);
+			parent.addOrReplaceChild("bug_" + (iteration * bugs + i), CubeListBuilder.create()
+							.texOffs(rand.nextInt(28), rand.nextInt(28))
+							.addBox(bugX, bugY, bugZ, 1, 1, 1),
+					PartPose.offsetAndRotation((float) vec.x, (float) vec.y, (float) vec.z, 0, rotateY, 0));
 		}
 	}
 
 	@Override
-	public void setupAnim(MosquitoSwarmEntity entity, float v, float v1, float v2, float v3, float v4) { }
+	public ModelPart root() {
+		return this.root;
+	}
+
+	@Override
+	public void setupAnim(MosquitoSwarmEntity entity, float v, float v1, float v2, float v3, float v4) {
+
+	}
 
 	@Override
 	public void prepareMobModel(MosquitoSwarmEntity entity, float limbSwing, float limbSwingAmount, float partialTicks) {
-		core.yRot = (entity.tickCount + partialTicks) / 5.0F;
-		core.xRot = Mth.sin((entity.tickCount + partialTicks) / 5.0F) / 4.0F;
-		core.zRot = Mth.cos((entity.tickCount + partialTicks) / 5.0F) / 4.0F;
+		this.core.yRot = (entity.tickCount + partialTicks) / 5.0F;
+		this.core.xRot = Mth.sin((entity.tickCount + partialTicks) / 5.0F) / 4.0F;
+		this.core.zRot = Mth.cos((entity.tickCount + partialTicks) / 5.0F) / 4.0F;
 
-		node1.yRot = (entity.tickCount + partialTicks) / 2.0F;
-		node1.xRot = Mth.sin((entity.tickCount + partialTicks) / 6.0F) / 2.0F;
-		node1.zRot = Mth.cos((entity.tickCount + partialTicks) / 5.0F) / 4.0F;
+		this.group1.yRot = (entity.tickCount + partialTicks) / 2.0F;
+		this.group1.xRot = Mth.sin((entity.tickCount + partialTicks) / 6.0F) / 2.0F;
+		this.group1.zRot = Mth.cos((entity.tickCount + partialTicks) / 5.0F) / 4.0F;
 
-		node2.yRot = Mth.sin((entity.tickCount + partialTicks) / 2.0F) / 3.0F;
-		node2.xRot = (entity.tickCount + partialTicks) / 5.0F;
-		node2.zRot = Mth.cos((entity.tickCount + partialTicks) / 5.0F) / 4.0F;
+		this.group2.yRot = Mth.sin((entity.tickCount + partialTicks) / 2.0F) / 3.0F;
+		this.group2.xRot = (entity.tickCount + partialTicks) / 5.0F;
+		this.group2.zRot = Mth.cos((entity.tickCount + partialTicks) / 5.0F) / 4.0F;
 
-		node3.yRot = Mth.sin((entity.tickCount + partialTicks) / 7.0F) / 3.0F;
-		node3.xRot = Mth.cos((entity.tickCount + partialTicks) / 4.0F) / 2.0F;
-		node3.zRot = (entity.tickCount + partialTicks) / 5.0F;
+		this.group3.yRot = Mth.sin((entity.tickCount + partialTicks) / 7.0F) / 3.0F;
+		this.group3.xRot = Mth.cos((entity.tickCount + partialTicks) / 4.0F) / 2.0F;
+		this.group3.zRot = (entity.tickCount + partialTicks) / 5.0F;
 
-		node4.xRot = (entity.tickCount + partialTicks) / 2.0F;
-		node4.zRot = Mth.sin((entity.tickCount + partialTicks) / 6.0F) / 2.0F;
-		node4.yRot = Mth.sin((entity.tickCount + partialTicks) / 5.0F) / 4.0F;
+		this.group4.xRot = (entity.tickCount + partialTicks) / 2.0F;
+		this.group4.zRot = Mth.sin((entity.tickCount + partialTicks) / 6.0F) / 2.0F;
+		this.group4.yRot = Mth.sin((entity.tickCount + partialTicks) / 5.0F) / 4.0F;
 
-		node5.zRot = (entity.tickCount + partialTicks) / 2.0F;
-		node5.yRot = Mth.cos((entity.tickCount + partialTicks) / 5.0F) / 4.0F;
-		node5.xRot = Mth.cos((entity.tickCount + partialTicks) / 5.0F) / 4.0F;
+		this.group5.zRot = (entity.tickCount + partialTicks) / 2.0F;
+		this.group5.yRot = Mth.cos((entity.tickCount + partialTicks) / 5.0F) / 4.0F;
+		this.group5.xRot = Mth.cos((entity.tickCount + partialTicks) / 5.0F) / 4.0F;
 
-		node6.zRot = Mth.cos((entity.tickCount + partialTicks) / 7.0F) / 3.0F;
-		node6.xRot = Mth.cos((entity.tickCount + partialTicks) / 4.0F) / 2.0F;
-		node6.yRot = (entity.tickCount + partialTicks) / 5.0F;
+		this.group6.zRot = Mth.cos((entity.tickCount + partialTicks) / 7.0F) / 3.0F;
+		this.group6.xRot = Mth.cos((entity.tickCount + partialTicks) / 4.0F) / 2.0F;
+		this.group6.yRot = (entity.tickCount + partialTicks) / 5.0F;
 	}
 }
--- src/main/java/twilightforest/client/model/entity/SnowQueenModel.java
+++ src/main/java/twilightforest/client/model/entity/SnowQueenModel.java
@@ -2,6 +2,11 @@ package twilightforest.client.model.entity;
 
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.model.geom.builders.MeshDefinition;
+import net.minecraft.client.model.geom.builders.PartDefinition;
 import net.minecraft.util.Mth;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -13,56 +18,70 @@ import twilightforest.entity.boss.SnowQueenEntity;
  */
 @OnlyIn(Dist.CLIENT)
 public class SnowQueenModel extends HumanoidModel<SnowQueenEntity> {
+    public SnowQueenModel(ModelPart root) {
+        super(root);
+    }
 
-    public ModelPart crownFront;
-    public ModelPart crownBack;
-    public ModelPart crownRight;
-    public ModelPart crownLeft;
+    public static LayerDefinition create() {
+        MeshDefinition mesh = new MeshDefinition();
+        PartDefinition partRoot = mesh.getRoot();
 
-    public SnowQueenModel() {
-        super(0, 0, 64, 64);
-        this.crownRight = new ModelPart(this, 0, 0);
-        this.crownRight.setPos(-4.0F, -6.0F, 0.0F);
-        this.crownRight.texOffs(24, 4).addBox(-5.0F, -4.0F, 0.0F, 10.0F, 4.0F, 0.0F, 0.0F, 0.0F, 0.0F);
-        this.setRotateAngle(crownRight, 0.39269908169872414F, 1.5707963267948966F, 0.0F);
-        this.crownBack = new ModelPart(this, 0, 0);
-        this.crownBack.setPos(0.0F, -6.0F, 4.0F);
-        this.crownBack.texOffs(44, 0).addBox(-5.0F, -4.0F, 0.0F, 10.0F, 4.0F, 0.0F, 0.0F, 0.0F, 0.0F);
-        this.setRotateAngle(crownBack, -0.39269908169872414F, 0.0F, 0.0F);
-        this.body = new ModelPart(this, 0, 0);
-        this.body.setPos(0.0F, 0.0F, 0.0F);
-        this.body.texOffs(0, 16).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, 0.0F, 0.0F, 0.0F);
-        this.body.texOffs(32, 45).addBox(-4.5F, 10.0F, -2.5F, 9.0F, 14.0F, 5.0F, 0.0F, 0.0F, 0.0F);
-        this.head = new ModelPart(this, 0, 0);
-        this.head.setPos(0.0F, 0.0F, 0.0F);
-        this.head.addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, 0.0F, 0.0F, 0.0F);
-        this.leftArm = new ModelPart(this, 0, 0);
-        this.leftArm.setPos(5.0F, 2.0F, 0.0F);
-        this.leftArm.texOffs(14, 32).addBox(-1.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, 0.0F, 0.0F, 0.0F);
-        this.leftLeg = new ModelPart(this, 0, 0);
-        this.leftLeg.setPos(1.9F, 12.0F, 0.0F);
-        this.leftLeg.texOffs(16, 48).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, 0.0F, 0.0F);
-        this.crownLeft = new ModelPart(this, 0, 0);
-        this.crownLeft.setPos(4.0F, -6.0F, 0.0F);
-        this.crownLeft.texOffs(44, 4).addBox(-5.0F, -4.0F, 0.0F, 10.0F, 4.0F, 0.0F, 0.0F, 0.0F, 0.0F);
-        this.setRotateAngle(crownLeft, -0.39269908169872414F, 1.5707963267948966F, 0.0F);
-        this.rightLeg = new ModelPart(this, 0, 0);
-        this.rightLeg.setPos(-1.9F, 12.0F, 0.0F);
-        this.rightLeg.texOffs(0, 48).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, 0.0F, 0.0F);
-        this.rightArm = new ModelPart(this, 0, 0);
-        this.rightArm.setPos(-5.0F, 2.0F, 0.0F);
-        this.rightArm.texOffs(0, 32).addBox(-2.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, 0.0F, 0.0F, 0.0F);
-        this.crownFront = new ModelPart(this, 0, 0);
-        this.crownFront.setPos(0.0F, -6.0F, -4.0F);
-        this.crownFront.texOffs(24, 0).addBox(-5.0F, -4.0F, 0.0F, 10.0F, 4.0F, 0.0F, 0.0F, 0.0F, 0.0F);
-        this.setRotateAngle(crownFront, 0.39269908169872414F, 0.0F, 0.0F);
-        this.hat = new ModelPart(this, 0, 0);
-        this.hat.addBox(0, 0, 0, 0, 0, 0);
+        partRoot.addOrReplaceChild("body", CubeListBuilder.create()
+                        .texOffs(0, 16)
+                        .addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F)
+                        .texOffs(32, 45)
+                        .addBox(-4.5F, 10.0F, -2.5F, 9.0F, 14.0F, 5.0F),
+                PartPose.ZERO);
 
-        this.hat.addChild(this.crownRight);
-        this.hat.addChild(this.crownBack);
-        this.hat.addChild(this.crownLeft);
-        this.hat.addChild(this.crownFront);
+        partRoot.addOrReplaceChild("head", CubeListBuilder.create()
+                        .addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F),
+                PartPose.ZERO);
+
+        partRoot.addOrReplaceChild("left_arm", CubeListBuilder.create()
+                        .texOffs(14, 32)
+                        .addBox(-1.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F),
+                PartPose.offset(5.0F, 2.0F, 0.0F));
+
+        partRoot.addOrReplaceChild("left_leg", CubeListBuilder.create()
+                        .texOffs(16, 48)
+                        .addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F),
+                PartPose.offset(1.9F, 12.0F, 0.0F));
+
+        partRoot.addOrReplaceChild("right_leg", CubeListBuilder.create()
+                        .texOffs(0, 48)
+                        .addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F),
+                PartPose.offset(-1.9F, 12.0F, 0.0F));
+
+        partRoot.addOrReplaceChild("right_arm", CubeListBuilder.create()
+                        .texOffs(0, 32)
+                        .addBox(-2.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F),
+                PartPose.offset(-5.0F, 2.0F, 0.0F));
+
+        var hat = partRoot.addOrReplaceChild("hat", CubeListBuilder.create()
+                        .addBox(0, 0, 0, 0, 0, 0),
+                PartPose.ZERO);
+
+        hat.addOrReplaceChild("crown_front", CubeListBuilder.create()
+                        .texOffs(24, 0)
+                        .addBox(-5.0F, -4.0F, 0.0F, 10.0F, 4.0F, 0.0F),
+                PartPose.offsetAndRotation(0.0F, -6.0F, -4.0F, 0.39269908169872414F, 0.0F, 0.0F));
+
+        hat.addOrReplaceChild("crown_right", CubeListBuilder.create()
+                        .texOffs(24, 4)
+                        .addBox(-5.0F, -4.0F, 0.0F, 10.0F, 4.0F, 0.0F),
+                PartPose.offsetAndRotation(-4.0F, -6.0F, 0.0F, 0.39269908169872414F, 1.5707963267948966F, 0.0F));
+
+        hat.addOrReplaceChild("crown_left", CubeListBuilder.create()
+                        .texOffs(44, 4)
+                        .addBox(-5.0F, -4.0F, 0.0F, 10.0F, 4.0F, 0.0F),
+                PartPose.offsetAndRotation(4.0F, -6.0F, 0.0F, -0.39269908169872414F, 1.5707963267948966F, 0.0F));
+
+        hat.addOrReplaceChild("crown_back", CubeListBuilder.create()
+                        .texOffs(44, 0)
+                        .addBox(-5.0F, -4.0F, 0.0F, 10.0F, 4.0F, 0.0F),
+                PartPose.offsetAndRotation(0.0F, -6.0F, 4.0F, -0.39269908169872414F, 0.0F, 0.0F));
+
+        return LayerDefinition.create(mesh, 64, 64);
     }
 
     @Override
@@ -72,14 +91,14 @@ public class SnowQueenModel extends HumanoidModel<SnowQueenEntity> {
         // in beam phase, arms forwards
         if (entity.getCurrentPhase() == SnowQueenEntity.Phase.BEAM) {
             if (entity.isBreathing()) {
-                float f6 = Mth.sin(this.attackTime * (float) Math.PI);
-                float f7 = Mth.sin((1.0F - (1.0F - this.attackTime) * (1.0F - this.attackTime)) * (float) Math.PI);
+                float f6 = Mth.sin(this.attackTime * Mth.PI);
+                float f7 = Mth.sin((1.0F - (1.0F - this.attackTime) * (1.0F - this.attackTime)) * Mth.PI);
                 this.rightArm.zRot = 0.0F;
                 this.leftArm.zRot = 0.0F;
                 this.rightArm.yRot = -(0.1F - f6 * 0.6F);
                 this.leftArm.yRot = 0.1F - f6 * 0.6F;
-                this.rightArm.xRot = -((float) Math.PI / 2F);
-                this.leftArm.xRot = -((float) Math.PI / 2F);
+                this.rightArm.xRot = -Mth.HALF_PI;
+                this.leftArm.xRot = -Mth.HALF_PI;
                 this.rightArm.xRot -= f6 * 1.2F - f7 * 0.4F;
                 this.leftArm.xRot -= f6 * 1.2F - f7 * 0.4F;
                 this.rightArm.zRot += Mth.cos(ageInTicks * 0.09F) * 0.05F + 0.05F;
@@ -88,17 +107,9 @@ public class SnowQueenModel extends HumanoidModel<SnowQueenEntity> {
                 this.leftArm.xRot -= Mth.sin(ageInTicks * 0.067F) * 0.05F;
             } else {
                 // arms up
-                this.rightArm.xRot += Math.PI;
-                this.leftArm.xRot += Math.PI;
+                this.rightArm.xRot += Mth.PI;
+                this.leftArm.xRot += Mth.PI;
             }
         }
-    }
-    /**
-     * This is a helper function from Tabula to set the rotation of model parts
-     */
-    public void setRotateAngle(ModelPart modelRenderer, float x, float y, float z) {
-        modelRenderer.xRot = x;
-        modelRenderer.yRot = y;
-        modelRenderer.zRot = z;
     }
 }
