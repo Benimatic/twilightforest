1a33182706b388667e0aabd742b2bb0ffeb771b1
Drullkus <failbookftw@gmail.com>
2021-08-12 19:50:12 -0700

    Forgot Copper for Ore Magnet tags

--- src/main/java/twilightforest/data/BlockTagGenerator.java
+++ src/main/java/twilightforest/data/BlockTagGenerator.java
@@ -306,21 +306,34 @@ public class BlockTagGenerator extends BlockTagsProvider {
                 .add(Blocks.STONE, Blocks.NETHERRACK, Blocks.END_STONE, TFBlocks.root.get(), Blocks.DEEPSLATE);
 
         // Don't add general ore taglists here, since those will include non-stone ores
-        tag(ORE_MAGNET_STONE)
-                .add(Blocks.GOLD_ORE, Blocks.IRON_ORE, Blocks.COAL_ORE, Blocks.LAPIS_ORE, Blocks.DIAMOND_ORE, Blocks.REDSTONE_ORE, Blocks.EMERALD_ORE);
+        tag(ORE_MAGNET_STONE).add(
+                Blocks.GOLD_ORE,
+                Blocks.IRON_ORE,
+                Blocks.COAL_ORE,
+                Blocks.LAPIS_ORE,
+                Blocks.DIAMOND_ORE,
+                Blocks.REDSTONE_ORE,
+                Blocks.EMERALD_ORE,
+                Blocks.COPPER_ORE
+        );
 
-        tag(ORE_MAGNET_NETHERRACK)
-                .add(Blocks.NETHER_GOLD_ORE, Blocks.NETHER_QUARTZ_ORE);
+        tag(ORE_MAGNET_NETHERRACK).add(Blocks.NETHER_GOLD_ORE, Blocks.NETHER_QUARTZ_ORE);
 
-        tag(ORE_MAGNET_END_STONE)
-                // Using a Quark ore as an example filler
-                .addOptional(new ResourceLocation("quark", "biotite_ore"));
+        // Using a Quark ore as an example filler
+        tag(ORE_MAGNET_END_STONE).addOptional(new ResourceLocation("quark", "biotite_ore"));
 
-        tag(ORE_MAGNET_DEEPSLATE)
-                .add(Blocks.DEEPSLATE_GOLD_ORE, Blocks.DEEPSLATE_IRON_ORE, Blocks.DEEPSLATE_COAL_ORE, Blocks.DEEPSLATE_LAPIS_ORE, Blocks.DEEPSLATE_DIAMOND_ORE, Blocks.DEEPSLATE_REDSTONE_ORE, Blocks.DEEPSLATE_EMERALD_ORE);
+        tag(ORE_MAGNET_DEEPSLATE).add(
+                Blocks.DEEPSLATE_GOLD_ORE,
+                Blocks.DEEPSLATE_IRON_ORE,
+                Blocks.DEEPSLATE_COAL_ORE,
+                Blocks.DEEPSLATE_LAPIS_ORE,
+                Blocks.DEEPSLATE_DIAMOND_ORE,
+                Blocks.DEEPSLATE_REDSTONE_ORE,
+                Blocks.DEEPSLATE_EMERALD_ORE,
+                Blocks.DEEPSLATE_COPPER_ORE
+        );
 
-        tag(ORE_MAGNET_ROOT)
-                .add(TFBlocks.liveroot_block.get());
+        tag(ORE_MAGNET_ROOT).add(TFBlocks.liveroot_block.get());
     }
 
     private static Block[] getAllMinecraftOrTwilightBlocks(Predicate<Block> predicate) {
