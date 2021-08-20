86bb625176c67442d9e7a005b483e912a4c05b36
Drullkus <failbookftw@gmail.com>
2021-08-20 12:39:31 -0700

    Implement a Wrapper for Surface Builders

--- src/main/java/twilightforest/world/components/surfacebuilders/GlacierSurfaceBuilder.java
+++ src/main/java/twilightforest/world/components/surfacebuilders/GlacierSurfaceBuilder.java
@@ -1,44 +1,58 @@
 package twilightforest.world.components.surfacebuilders;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkAccess;
-import net.minecraft.world.level.levelgen.surfacebuilders.DefaultSurfaceBuilder;
-import net.minecraft.world.level.levelgen.surfacebuilders.SurfaceBuilderBaseConfiguration;
-import twilightforest.world.registration.BlockConstants;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.surfacebuilders.ConfiguredSurfaceBuilder;
 
 import java.util.Random;
+import java.util.function.Supplier;
 
-// TODO set up custom config for blocks and heights and delegation to different surface builder
-public class GlacierSurfaceBuilder extends DefaultSurfaceBuilder {
-	public GlacierSurfaceBuilder(Codec<SurfaceBuilderBaseConfiguration> config) {
+public class GlacierSurfaceBuilder extends DelegatingSurfaceBuilder<GlacierSurfaceBuilder.GlacierSurfaceConfig> {
+	public GlacierSurfaceBuilder(Codec<GlacierSurfaceBuilder.GlacierSurfaceConfig> config) {
 		super(config);
 	}
 
 	@Override
-	public void apply(Random random, ChunkAccess chunkIn, Biome biomeIn, int x, int z, int startHeight, double noise, BlockState defaultBlock, BlockState defaultFluid, int seaLevel, int minSurfaceLevel, long seed, SurfaceBuilderBaseConfiguration config) {
-		super.apply(random, chunkIn, biomeIn, x, z, startHeight, noise, defaultBlock, defaultFluid, seaLevel, minSurfaceLevel, seed, config);
+	public void apply(Random random, ChunkAccess chunkIn, Biome biomeIn, int x, int z, int startHeight, double noise, BlockState defaultBlock, BlockState defaultFluid, int seaLevel, int minSurfaceLevel, long seed, GlacierSurfaceBuilder.GlacierSurfaceConfig config) {
+		config.surfaceBuilder.apply(random, chunkIn, biomeIn, x, z, startHeight, noise, defaultBlock, defaultFluid, seaLevel, minSurfaceLevel, seed);
 
-		final BlockPos.MutableBlockPos mutablePos = new BlockPos.MutableBlockPos(x, startHeight - 1, z);
+		final int oceanFloorHeight = chunkIn.getHeight(Heightmap.Types.OCEAN_FLOOR_WG, x, z);
+		final int depthStart = Math.min(startHeight, oceanFloorHeight);
+		final int targetHeight = Math.max(seaLevel, oceanFloorHeight) + config.mainHeight;
 
-		for (int y = 0; y < 30; y++) {
-			chunkIn.setBlockState(mutablePos, BlockConstants.PACKED_ICE, false);
-			mutablePos.move(0, 1, 0);
+		final BlockPos.MutableBlockPos mutablePos = new BlockPos.MutableBlockPos(x, depthStart, z);
+
+		for (int y = depthStart; y < targetHeight; y++) {
+			chunkIn.setBlockState(mutablePos.setY(y), config.mainState, false);
 		}
 
-		for (int y = 0; y < 2; y++) {
-			chunkIn.setBlockState(mutablePos, BlockConstants.ICE, false);
-			mutablePos.move(0, 1, 0);
+		for (int y = 0; y < config.topHeight; y++) {
+			chunkIn.setBlockState(mutablePos.move(0, 1, 0), config.topState, false);
 		}
+	}
 
-		for (int y = startHeight; y >= minSurfaceLevel; y--) {
-			if (chunkIn.getBlockState(mutablePos).getMaterial().isReplaceable()) {
-				chunkIn.setBlockState(mutablePos, BlockConstants.PACKED_ICE, false);
-			} else {
-				return; // We're done here, return instead of breaking
-			}
+	public static class GlacierSurfaceConfig extends DelegatingSurfaceBuilder.DelegatingConfig {
+		public static Codec<GlacierSurfaceConfig> CODEC = RecordCodecBuilder.create(instance -> DelegatingSurfaceBuilder.DelegatingConfig.startCodec(instance).and(instance.group(
+				Codec.intRange(1, 64).fieldOf("main_thickness").forGetter(o -> o.mainHeight),
+				Codec.intRange(1, 16).fieldOf("top_thickness").forGetter(o -> o.topHeight),
+				BlockState.CODEC.fieldOf("main_state").forGetter(o -> o.mainState),
+				BlockState.CODEC.fieldOf("top_state").forGetter(o -> o.topState)
+		)).apply(instance, GlacierSurfaceConfig::new));
+
+		private final int mainHeight, topHeight;
+		private final BlockState mainState, topState;
+
+		public GlacierSurfaceConfig(Supplier<ConfiguredSurfaceBuilder<?>> configuredSurfaceBuilderSupplier, int mainHeight, int topHeight, BlockState mainState, BlockState topState) {
+			super(configuredSurfaceBuilderSupplier);
+			this.mainHeight = mainHeight;
+			this.topHeight = topHeight;
+			this.mainState = mainState;
+			this.topState = topState;
 		}
 	}
 }
--- src/main/java/twilightforest/world/registration/TwilightSurfaceBuilders.java
+++ src/main/java/twilightforest/world/registration/TwilightSurfaceBuilders.java
@@ -14,7 +14,7 @@ import twilightforest.world.components.surfacebuilders.HighlandsSurfaceBuilder;
 public class TwilightSurfaceBuilders {
 	// Biomes are registered before surface builders and need the raw objects. So don't use DeferredRegister here.
 	public static final FillingSurfaceBuilder DEADROCK_FILLING = new FillingSurfaceBuilder(FillingSurfaceBuilder.FillingSurfaceBuilderConfig.CODEC);
-	public static final GlacierSurfaceBuilder GLACIER = new GlacierSurfaceBuilder(SurfaceBuilderBaseConfiguration.CODEC);
+	public static final GlacierSurfaceBuilder GLACIER = new GlacierSurfaceBuilder(GlacierSurfaceBuilder.GlacierSurfaceConfig.CODEC);
 
 	// TODO Can we move off of this and use surface features instead?
 	public static final HighlandsSurfaceBuilder HIGHLANDS = new HighlandsSurfaceBuilder(SurfaceBuilderBaseConfiguration.CODEC);
@@ -22,7 +22,7 @@ public class TwilightSurfaceBuilders {
 	static {
 		// setRegistryName for some dumb reason erases the type for SurfaceBuilder<> and we have to keep the type for later in the line, so the reg names are kept here instead
 		DEADROCK_FILLING.setRegistryName(TwilightForestMod.ID, "plateau");
-		GLACIER.setRegistryName(TwilightForestMod.ID, "glacer");
+		GLACIER.setRegistryName(TwilightForestMod.ID, "glacier");
 		HIGHLANDS.setRegistryName(TwilightForestMod.ID, "highlands");
 	}
 
