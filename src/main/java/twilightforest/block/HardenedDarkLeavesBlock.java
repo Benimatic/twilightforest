f728e563cad57578e7a08fe077d82e925f18624f
GizmoTheMoonPig <cnicholasb@comcast.net>
2021-09-18 16:34:51 -0600

    fixed hardened dark leaves block picking returning null item

--- src/main/java/twilightforest/block/HardenedDarkLeavesBlock.java
+++ src/main/java/twilightforest/block/HardenedDarkLeavesBlock.java
@@ -2,9 +2,12 @@ package twilightforest.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.HitResult;
 
 public class HardenedDarkLeavesBlock extends Block {
 
@@ -22,4 +25,8 @@ public class HardenedDarkLeavesBlock extends Block {
 		return 0;
 	}
 
+	@Override
+	public ItemStack getPickBlock(BlockState state, HitResult target, BlockGetter world, BlockPos pos, Player player) {
+		return new ItemStack(TFBlocks.dark_leaves.get());
+	}
 }
