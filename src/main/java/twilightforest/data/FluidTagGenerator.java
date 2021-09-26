75ab139d7af8fe68c77e1765c0de70759d189a49
Drullkus <failbookftw@gmail.com>
2021-09-25 22:01:32 -0700

    Turns out the fluid tag was unused anyway, addresses #1278

--- src/main/java/twilightforest/data/FluidTagGenerator.java
+++ src/main/java/twilightforest/data/FluidTagGenerator.java
@@ -13,7 +13,6 @@ import javax.annotation.Nullable;
 public class FluidTagGenerator extends FluidTagsProvider {
 
     public static final Tag.Named<Fluid> FIRE_JET_FUEL = FluidTags.bind(TwilightForestMod.prefix("fire_jet_fuel").toString());
-    public static final Tag.Named<Fluid> PORTAL_FLUID = FluidTags.bind(TwilightForestMod.prefix("portal_fluid").toString());
 
     public FluidTagGenerator(DataGenerator generatorIn, @Nullable ExistingFileHelper existingFileHelper) {
         super(generatorIn, TwilightForestMod.ID, existingFileHelper);
@@ -22,6 +21,5 @@ public class FluidTagGenerator extends FluidTagsProvider {
     @Override
     protected void addTags() {
         tag(FIRE_JET_FUEL).addTag(FluidTags.LAVA);
-        tag(PORTAL_FLUID).addTag(FluidTags.WATER);
     }
 }
