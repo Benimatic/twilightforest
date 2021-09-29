2af9802298c161fb4a9ca8d4b81969d21aa68374
Tamaized <9671313+Tamaized@users.noreply.github.com>
2021-09-29 12:19:49 -0400

    absolutely terrible bandaid fix for rising zombie render
    Signed-off-by: Tamaized <9671313+Tamaized@users.noreply.github.com>

--- src/main/java/twilightforest/client/model/entity/RisingZombieModel.java
+++ src/main/java/twilightforest/client/model/entity/RisingZombieModel.java
@@ -2,11 +2,15 @@ package twilightforest.client.model.entity;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Vector3f;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ZombieModel;
 import net.minecraft.client.model.geom.ModelPart;
 import twilightforest.entity.monster.RisingZombie;
 
 public class RisingZombieModel extends ZombieModel<RisingZombie> {
+
+	private float tick;
 
 	public RisingZombieModel(ModelPart part) {
 		super(part);
@@ -38,18 +42,16 @@ public class RisingZombieModel extends ZombieModel<RisingZombie> {
 				stack.translate(0.0F, 0.2F, 0.0F);
 			}
 
-			/* todo 1.15 ageInTicks/the entity only provided to setRotationAngles now, rework this entire render and move this transform there
-			stack.translate(0F, (80F - Math.min(80F, ageInTicks)) / 80F, 0F);
-			stack.translate(0F, (40F - Math.min(40F, Math.max(0F, ageInTicks - 80F))) / 40F, 0F);
-				 */
+			// todo 1.15 ageInTicks/the entity only provided to setRotationAngles now, rework this entire render and move this transform there
+			stack.translate(0F, (80F - Math.min(80F, tick)) / 80F, 0F);
+			stack.translate(0F, (40F - Math.min(40F, Math.max(0F, tick - 80F))) / 40F, 0F);
 			stack.pushPose();
 			{
 				final float yOff = 1F;
 				stack.translate(0, yOff, 0);
-				/* todo 1.15 ageInTicks/the entity only provided to setRotationAngles now, rework this entire render and move this transform there
-				RenderSystem.rotatef(-120F * (80F - Math.min(80F, ageInTicks)) / 80F, 1F, 0F, 0F);
-				RenderSystem.rotatef(30F * (40F - Math.min(40F, Math.max(0F, ageInTicks - 80F))) / 40F, 1F, 0F, 0F);
-				 */
+				// todo 1.15 ageInTicks/the entity only provided to setRotationAngles now, rework this entire render and move this transform there
+				stack.mulPose(Vector3f.XP.rotationDegrees(-120F * (80F - Math.min(80F, tick)) / 80F));
+				stack.mulPose(Vector3f.XP.rotationDegrees(30F * (40F - Math.min(40F, Math.max(0F, tick - 80F))) / 40F));
 				stack.translate(0, -yOff, 0);
 				this.headParts().forEach((renderer) -> renderer.render(stack, builder, light, overlay, red, green, blue, scale));
 				this.body.render(stack, builder, light, overlay, red, green, blue, scale);
@@ -63,5 +65,11 @@ public class RisingZombieModel extends ZombieModel<RisingZombie> {
 		}
 
 		stack.popPose();
+	}
+
+	@Override
+	protected void setupAttackAnimation(RisingZombie p_102858_, float p_102859_) {
+		super.setupAttackAnimation(p_102858_, p_102859_);
+		tick = p_102859_ + Minecraft.getInstance().getDeltaFrameTime();
 	}
 }
