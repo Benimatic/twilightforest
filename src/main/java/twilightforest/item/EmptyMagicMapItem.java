35f9a917952138beffd6ac4e6dfce7edb4fa52d8
Tamaized <9671313+Tamaized@users.noreply.github.com>
2021-09-28 20:29:46 -0400

    disallow magic maps from being created in non TF-like dims, closes #1308
    Signed-off-by: Tamaized <9671313+Tamaized@users.noreply.github.com>

--- src/main/java/twilightforest/item/EmptyMagicMapItem.java
+++ src/main/java/twilightforest/item/EmptyMagicMapItem.java
@@ -1,15 +1,17 @@
 package twilightforest.item;
 
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.Mth;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ComplexItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.stats.Stats;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.util.Mth;
 import net.minecraft.world.level.Level;
-
-import net.minecraft.world.item.Item.Properties;
+import twilightforest.TwilightForestMod;
+import twilightforest.world.registration.TFGenerationSettings;
 
 public class EmptyMagicMapItem extends ComplexItem {
 	protected EmptyMagicMapItem(Properties props) {
@@ -19,22 +21,28 @@ public class EmptyMagicMapItem extends ComplexItem {
 	// [VanillaCopy] ItemEmptyMap.onItemRightClick, edits noted
 	@Override
 	public InteractionResultHolder<ItemStack> use(Level worldIn, Player playerIn, InteractionHand handIn) {
+		ItemStack emptyMapStack = playerIn.getItemInHand(handIn);
+		if (worldIn.isClientSide)
+			return InteractionResultHolder.pass(emptyMapStack);
+		if(worldIn instanceof ServerLevel level && !TFGenerationSettings.usesTwilightChunkGenerator(level)) {
+			playerIn.displayClientMessage(new TranslatableComponent(TwilightForestMod.ID + ".ui.magicmap.fail"), true);
+			return InteractionResultHolder.fail(emptyMapStack);
+		}
 		// TF - scale at 4
-		ItemStack itemstack = MagicMapItem.setupNewMap(worldIn, Mth.floor(playerIn.getX()), Mth.floor(playerIn.getZ()), (byte) 4, true, false);
-		ItemStack itemstack1 = playerIn.getItemInHand(handIn);
+		ItemStack newMapStack = MagicMapItem.setupNewMap(worldIn, Mth.floor(playerIn.getX()), Mth.floor(playerIn.getZ()), (byte) 4, true, false);
 		if (!playerIn.getAbilities().instabuild) {
-			itemstack1.shrink(1);
+			emptyMapStack.shrink(1);
 		}
 
-		if (itemstack1.isEmpty()) {
-			return InteractionResultHolder.success(itemstack);
+		if (emptyMapStack.isEmpty()) {
+			return InteractionResultHolder.success(newMapStack);
 		} else {
-			if (!playerIn.getInventory().add(itemstack.copy())) {
-				playerIn.drop(itemstack, false);
+			if (!playerIn.getInventory().add(newMapStack.copy())) {
+				playerIn.drop(newMapStack, false);
 			}
 
 			playerIn.awardStat(Stats.ITEM_USED.get(this));
-			return InteractionResultHolder.success(itemstack1);
+			return InteractionResultHolder.success(emptyMapStack);
 		}
 	}
 }
