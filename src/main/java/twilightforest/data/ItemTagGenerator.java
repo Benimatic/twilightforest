ca0ce23cb50871b10705b6462454191af6eb2ecd
GizmoTheMoonPig <cnicholasb@comcast.net>
2021-11-29 00:45:09 -0700

    oh yeah I can do this now

--- src/main/java/twilightforest/data/ItemTagGenerator.java
+++ src/main/java/twilightforest/data/ItemTagGenerator.java
@@ -213,7 +213,7 @@ public class ItemTagGenerator extends ItemTagsProvider {
 				TFBlocks.MINING_SAPLING.get().asItem(),
 				TFBlocks.SORTING_SAPLING.get().asItem());
 
-		tag(BANNED_UNCRAFTABLES).add(TFItems.MOONWORM_QUEEN.get());
+		tag(BANNED_UNCRAFTABLES).add(TFBlocks.GIANT_LOG.get().asItem());
 
 		tag(ItemTags.PIGLIN_LOVED).add(TFItems.GOLDEN_MINOTAUR_AXE.get(), TFItems.CHARM_OF_KEEPING_3.get(), TFItems.CHARM_OF_LIFE_2.get(), TFItems.LAMP_OF_CINDERS.get());
 	}
