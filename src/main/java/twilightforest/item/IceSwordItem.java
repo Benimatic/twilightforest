5ae9a110b0869d0d937f61fb44ade2c9833d51d5
GizmoTheMoonPig <cnicholasb@comcast.net>
2022-04-09 09:44:12 -0600

    fixed broken ice sword particle effect

--- src/main/java/twilightforest/item/IceSwordItem.java
+++ src/main/java/twilightforest/item/IceSwordItem.java
@@ -1,5 +1,6 @@
 package twilightforest.item;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.ItemStack;
@@ -19,10 +20,9 @@ public class IceSwordItem extends SwordItem {
 		boolean result = super.hurtEnemy(stack, target, attacker);
 
 		if (result) {
-			if (!target.level.isClientSide) {
-				target.addEffect(new MobEffectInstance(TFMobEffects.FROSTY.get(), 20 * 10, 2));
-			} else {
-				target.level.addParticle(TFParticleType.SNOW.get(), target.getX(), target.getY() + target.getBbHeight() * 0.5, target.getZ(), target.getBbWidth() * 0.5, target.getBbHeight() * 0.5, target.getBbWidth() * 0.5);
+			target.addEffect(new MobEffectInstance(TFMobEffects.FROSTY.get(), 20 * 10, 2));
+			for (int i = 0; i < 20; i++) {
+				((ServerLevel)target.level).sendParticles(TFParticleType.SNOW.get(), target.getX(), target.getY() + target.getBbHeight() * 0.5F, target.getZ(), 1, target.getBbWidth() * 0.5, target.getBbHeight() * 0.5, target.getBbWidth() * 0.5, 0);
 			}
 		}
 
