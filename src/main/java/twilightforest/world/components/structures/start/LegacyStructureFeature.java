62c74187f7623ade006019acb0892fa9fdf5fa9c
GizmoTheMoonPig <cnicholasb@comcast.net>
2022-04-23 23:20:26 -0600

    fixed our structures existing in other dimensions

--- src/main/java/twilightforest/world/components/structures/start/LegacyStructureFeature.java
+++ src/main/java/twilightforest/world/components/structures/start/LegacyStructureFeature.java
@@ -1,22 +1,37 @@
 package twilightforest.world.components.structures.start;
 
+import net.minecraft.core.Holder;
+import net.minecraft.core.QuartPos;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
+import net.minecraft.world.level.levelgen.structure.pieces.PieceGeneratorSupplier;
+import twilightforest.TwilightForestMod;
 import twilightforest.world.registration.TFFeature;
+
+import java.util.Objects;
+import java.util.Optional;
 
 @Deprecated
 public class LegacyStructureFeature extends TwilightStructureFeature<NoneFeatureConfiguration> {
     public final TFFeature feature;
 
     public LegacyStructureFeature(@Deprecated TFFeature feature) {
-        super(NoneFeatureConfiguration.CODEC, configContext -> feature.generatePieces(configContext).map(piece -> (structurePiecesBuilder, context) -> {
+        super(NoneFeatureConfiguration.CODEC, configContext -> isValidBiome(configContext) ? feature.generatePieces(configContext).map(piece -> (structurePiecesBuilder, context) -> {
 			piece.addChildren(piece, structurePiecesBuilder, context.random());
 			structurePiecesBuilder.addPiece(piece);
-		}));
+		}) : Optional.empty());
         this.feature = feature;
     }
 
-    //Temporally fixed Generation step error
+	private static boolean isValidBiome(PieceGeneratorSupplier.Context<NoneFeatureConfiguration> context) {
+		int x = context.chunkPos().getMiddleBlockX();
+		int z = context.chunkPos().getMiddleBlockZ();
+		int y = 1;
+		Holder<Biome> holder = context.chunkGenerator().getNoiseBiome(QuartPos.fromBlock(x), QuartPos.fromBlock(y), QuartPos.fromBlock(z));
+		return Objects.equals(holder.value().getRegistryName().getNamespace(), TwilightForestMod.ID);
+	}
+
     @Override
     public GenerationStep.Decoration step() {
         return feature.getDecorationStage();
