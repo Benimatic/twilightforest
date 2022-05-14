c2a5c1219d3802657ec03561f73a72dd509e1928
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2022-05-13 19:42:54 -0600

    prevent fiery sword from accepting fire aspect, it already has it built in

--- src/main/java/twilightforest/item/FierySwordItem.java
+++ src/main/java/twilightforest/item/FierySwordItem.java
@@ -9,6 +9,9 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -19,6 +22,8 @@ import twilightforest.TwilightForestMod;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Mod.EventBusSubscriber(modid = TwilightForestMod.ID)
 public class FierySwordItem extends SwordItem {
@@ -28,17 +33,34 @@ public class FierySwordItem extends SwordItem {
 	}
 
 	@Override
+	public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
+		return enchantment != Enchantments.FIRE_ASPECT && super.canApplyAtEnchantingTable(stack, enchantment);
+	}
+
+	@Override
+	public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
+		Map<Enchantment, Integer> enchants = EnchantmentHelper.getEnchantments(book);
+
+		for (Enchantment ench : enchants.keySet()) {
+			if (Objects.equals(ench.getRegistryName(), Enchantments.FIRE_ASPECT.getRegistryName())) {
+				return false;
+			}
+		}
+		return false;
+	}
+
+	@Override
 	public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
 		boolean result = super.hurtEnemy(stack, target, attacker);
 
-		if (result && !target.level.isClientSide && !target.fireImmune()) {
+		if (result && !target.getLevel().isClientSide() && !target.fireImmune()) {
 			target.setSecondsOnFire(15);
 		} else {
 			for (int var1 = 0; var1 < 20; ++var1) {
-				double px = target.getX() + target.level.random.nextFloat() * target.getBbWidth() * 2.0F - target.getBbWidth();
-				double py = target.getY() + target.level.random.nextFloat() * target.getBbHeight();
-				double pz = target.getZ() + target.level.random.nextFloat() * target.getBbWidth() * 2.0F - target.getBbWidth();
-				target.level.addParticle(ParticleTypes.FLAME, px, py, pz, 0.02, 0.02, 0.02);
+				double px = target.getX() + target.getLevel().getRandom().nextFloat() * target.getBbWidth() * 2.0F - target.getBbWidth();
+				double py = target.getY() + target.getLevel().getRandom().nextFloat() * target.getBbHeight();
+				double pz = target.getZ() + target.getLevel().getRandom().nextFloat() * target.getBbWidth() * 2.0F - target.getBbWidth();
+				target.getLevel().addParticle(ParticleTypes.FLAME, px, py, pz, 0.02, 0.02, 0.02);
 			}
 		}
 
@@ -48,15 +70,15 @@ public class FierySwordItem extends SwordItem {
 	//we have to set the entity on fire early in order to actually cook the food
 	@SubscribeEvent
 	public static void setFireBeforeDeath(LivingAttackEvent event) {
-		if(event.getSource().getEntity() instanceof LivingEntity living && living.getMainHandItem().is(TFItems.FIERY_SWORD.get())) {
+		if(event.getSource().getEntity() instanceof LivingEntity living && living.getMainHandItem().is(TFItems.FIERY_SWORD.get()) && !event.getEntityLiving().fireImmune()) {
 			event.getEntityLiving().setSecondsOnFire(1);
 		}
 	}
 
 	@Override
 	@OnlyIn(Dist.CLIENT)
-	public void appendHoverText(ItemStack stack, @Nullable Level world, List<Component> tooltip, TooltipFlag flags) {
-		super.appendHoverText(stack, world, tooltip, flags);
+	public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flag) {
+		super.appendHoverText(stack, level, tooltip, flag);
 		tooltip.add(new TranslatableComponent(getDescriptionId() + ".tooltip").withStyle(ChatFormatting.GRAY));
 	}
 }
