24d16aaa6a38be31576353fb6153896eef1b32fa
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2022-06-27 20:02:58 -0600

    fixed locked biome toasts

--- src/main/java/twilightforest/client/LockedBiomeListener.java
+++ src/main/java/twilightforest/client/LockedBiomeListener.java
@@ -35,23 +35,23 @@ public class LockedBiomeListener {
 			if (!TFGenerationSettings.isBiomeSafeFor(world.getBiome(player.blockPosition()).value(), player)) {
 				timeUntilToast--;
 				ItemStack item;
-				if (world.getBiome(player.blockPosition()).equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.DARK_FOREST))) {
+				if (world.getBiome(player.blockPosition()).value().equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.DARK_FOREST))) {
 					item = new ItemStack(TFBlocks.LICH_TOWER_MINIATURE_STRUCTURE.get());
-				} else if (world.getBiome(player.blockPosition()).equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.DARK_FOREST_CENTER))) {
+				} else if (world.getBiome(player.blockPosition()).value().equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.DARK_FOREST_CENTER))) {
 					item = new ItemStack(TFBlocks.KNIGHT_PHANTOM_TROPHY.get().asItem());
-				} else if (world.getBiome(player.blockPosition()).equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.SNOWY_FOREST))) {
+				} else if (world.getBiome(player.blockPosition()).value().equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.SNOWY_FOREST))) {
 					item = new ItemStack(TFBlocks.LICH_TOWER_MINIATURE_STRUCTURE.get());
-				} else if (world.getBiome(player.blockPosition()).equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.GLACIER))) {
+				} else if (world.getBiome(player.blockPosition()).value().equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.GLACIER))) {
 					item = new ItemStack(TFItems.ALPHA_YETI_FUR.get());
-				} else if (world.getBiome(player.blockPosition()).equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.SWAMP))) {
+				} else if (world.getBiome(player.blockPosition()).value().equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.SWAMP))) {
 					item = new ItemStack(TFBlocks.LICH_TOWER_MINIATURE_STRUCTURE.get());
-				} else if (world.getBiome(player.blockPosition()).equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.FIRE_SWAMP))) {
+				} else if (world.getBiome(player.blockPosition()).value().equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.FIRE_SWAMP))) {
 					item = new ItemStack(TFItems.MEEF_STROGANOFF.get());
-				} else if (world.getBiome(player.blockPosition()).equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.HIGHLANDS))) {
+				} else if (world.getBiome(player.blockPosition()).value().equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.HIGHLANDS))) {
 					item = new ItemStack(TFBlocks.UBEROUS_SOIL.get());
-				} else if (world.getBiome(player.blockPosition()).equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.THORNLANDS))) {
+				} else if (world.getBiome(player.blockPosition()).value().equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.THORNLANDS))) {
 					item = new ItemStack(TFItems.LAMP_OF_CINDERS.get());
-				} else if (world.getBiome(player.blockPosition()).equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.FINAL_PLATEAU))) {
+				} else if (world.getBiome(player.blockPosition()).value().equals(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).get(BiomeKeys.FINAL_PLATEAU))) {
 					item = new ItemStack(TFItems.LAMP_OF_CINDERS.get());
 				} else {
 					item = new ItemStack(TFBlocks.TWILIGHT_PORTAL_MINIATURE_STRUCTURE.get());
