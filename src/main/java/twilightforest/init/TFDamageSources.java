650599ff7e42d560b3e28de436d86884ede0ccce
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2022-06-30 21:47:22 -0600

    fixed hydra flames not counting as fire damage

--- src/main/java/twilightforest/init/TFDamageSources.java
+++ src/main/java/twilightforest/init/TFDamageSources.java
@@ -11,7 +11,7 @@ import org.jetbrains.annotations.Nullable;
 public class TFDamageSources {
     public static final DamageSource GHAST_TEAR = new DamageSource(tfSource("ghastTear")).bypassArmor(); //ur-ghast
     public static final DamageSource HYDRA_BITE = new DamageSource(tfSource("hydraBite")); //hydra
-    public static final DamageSource HYDRA_FIRE = new DamageSource(tfSource("hydraFire")); //hydra
+    public static final DamageSource HYDRA_FIRE = new DamageSource(tfSource("hydraFire")).setIsFire(); //hydra
     public static final DamageSource LICH_BOLT = new DamageSource(tfSource("lichBolt")).setProjectile().bypassArmor().setMagic(); //lich
     public static final DamageSource LICH_BOMB = new DamageSource(tfSource("lichBomb")).setProjectile().bypassArmor().setMagic(); //lich
     public static final DamageSource CHILLING_BREATH = new DamageSource(tfSource("chillingBreath")); //snow queen
