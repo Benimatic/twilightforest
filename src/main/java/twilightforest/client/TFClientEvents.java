173c04088ea8c0085b7eecea6a02b1ec94168f03
Tama <9671313+Tamaized@users.noreply.github.com>
2022-07-29 12:44:50 -0400

    Don't touch the visibility state when we don't need to be.

--- src/main/java/twilightforest/client/TFClientEvents.java
+++ src/main/java/twilightforest/client/TFClientEvents.java
@@ -333,10 +333,10 @@ public class TFClientEvents {
 		ItemStack stack = event.getEntity().getItemBySlot(EquipmentSlot.HEAD);
 		boolean visible = !(stack.getItem() instanceof TrophyItem) && !(stack.getItem() instanceof SkullCandleItem) && !areCuriosEquipped(event.getEntity());
 
-		if (event.getRenderer().getModel() instanceof HeadedModel headedModel) {
-			headedModel.getHead().visible = visible;
+		if (!visible && event.getRenderer().getModel() instanceof HeadedModel headedModel) {
+			headedModel.getHead().visible = false;
 			if (event.getRenderer().getModel() instanceof HumanoidModel<?> humanoidModel) {
-				humanoidModel.hat.visible = visible && partShown(event.getEntity());
+				humanoidModel.hat.visible = false;
 			}
 		}
 	}
