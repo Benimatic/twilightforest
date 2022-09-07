27bb16b8f72b7f0785e686c3e03246142fab37ce
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2022-09-07 07:18:36 -0600

    removed our biomes from vanilla and forge tags

--- src/main/java/twilightforest/data/tags/BiomeTagGenerator.java
+++ src/main/java/twilightforest/data/tags/BiomeTagGenerator.java
@@ -6,7 +6,6 @@ import net.minecraft.data.tags.BiomeTagsProvider;
 import net.minecraft.tags.BiomeTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import twilightforest.TwilightForestMod;
 import twilightforest.init.BiomeKeys;
@@ -88,34 +87,35 @@ public class BiomeTagGenerator extends BiomeTagsProvider {
 		tag(VALID_TROLL_CAVE_BIOMES).add(BiomeKeys.HIGHLANDS);
 		tag(VALID_FINAL_CASTLE_BIOMES).add(BiomeKeys.FINAL_PLATEAU);
 
+		//apparently using forge and vanilla tags allows other mods to spawn stuff in our biomes. Will keep these commented out here just in case we need to reference them later.
 		//vanilla biome categories
-		tag(BiomeTags.IS_FOREST).add(
-				BiomeKeys.FOREST, BiomeKeys.DENSE_FOREST, BiomeKeys.FIREFLY_FOREST,
-				BiomeKeys.OAK_SAVANNAH, BiomeKeys.MUSHROOM_FOREST, BiomeKeys.DENSE_MUSHROOM_FOREST,
-				BiomeKeys.DARK_FOREST, BiomeKeys.DARK_FOREST_CENTER,
-				BiomeKeys.SNOWY_FOREST, BiomeKeys.HIGHLANDS
-		);
-		tag(BiomeTags.IS_MOUNTAIN).add(BiomeKeys.HIGHLANDS);
-		tag(BiomeTags.IS_HILL).add(BiomeKeys.THORNLANDS);
+//		tag(BiomeTags.IS_FOREST).add(
+//				BiomeKeys.FOREST, BiomeKeys.DENSE_FOREST, BiomeKeys.FIREFLY_FOREST,
+//				BiomeKeys.OAK_SAVANNAH, BiomeKeys.MUSHROOM_FOREST, BiomeKeys.DENSE_MUSHROOM_FOREST,
+//				BiomeKeys.DARK_FOREST, BiomeKeys.DARK_FOREST_CENTER,
+//				BiomeKeys.SNOWY_FOREST, BiomeKeys.HIGHLANDS
+//		);
+//		tag(BiomeTags.IS_MOUNTAIN).add(BiomeKeys.HIGHLANDS);
+//		tag(BiomeTags.IS_HILL).add(BiomeKeys.THORNLANDS);
 
 		//forge biome categories
-		tag(Tags.Biomes.IS_DENSE).add(BiomeKeys.DENSE_FOREST, BiomeKeys.DENSE_MUSHROOM_FOREST, BiomeKeys.DARK_FOREST, BiomeKeys.DARK_FOREST_CENTER, BiomeKeys.SNOWY_FOREST, BiomeKeys.THORNLANDS);
-		tag(Tags.Biomes.IS_SPARSE).add(BiomeKeys.CLEARING, BiomeKeys.OAK_SAVANNAH, BiomeKeys.GLACIER, BiomeKeys.FINAL_PLATEAU);
-		tag(Tags.Biomes.IS_PLAINS).add(BiomeKeys.CLEARING);
-		tag(Tags.Biomes.IS_MUSHROOM).add(BiomeKeys.MUSHROOM_FOREST, BiomeKeys.DENSE_MUSHROOM_FOREST);
-		tag(Tags.Biomes.IS_RARE).add(BiomeKeys.ENCHANTED_FOREST, BiomeKeys.SPOOKY_FOREST, BiomeKeys.CLEARING, BiomeKeys.DENSE_MUSHROOM_FOREST, BiomeKeys.LAKE);
-		tag(Tags.Biomes.IS_WATER).add(BiomeKeys.LAKE, BiomeKeys.STREAM);
-		tag(Tags.Biomes.IS_MAGICAL).add(BiomeKeys.ENCHANTED_FOREST, BiomeKeys.DARK_FOREST_CENTER);
-		tag(Tags.Biomes.IS_SPOOKY).add(BiomeKeys.SPOOKY_FOREST, BiomeKeys.DARK_FOREST, BiomeKeys.DARK_FOREST_CENTER);
-		tag(Tags.Biomes.IS_DEAD).add(BiomeKeys.SPOOKY_FOREST, BiomeKeys.THORNLANDS, BiomeKeys.FINAL_PLATEAU);
-		tag(Tags.Biomes.IS_SWAMP).add(BiomeKeys.SWAMP, BiomeKeys.FIRE_SWAMP);
-		tag(Tags.Biomes.IS_SNOWY).add(BiomeKeys.SNOWY_FOREST);
-		tag(Tags.Biomes.IS_CONIFEROUS).add(BiomeKeys.SNOWY_FOREST, BiomeKeys.HIGHLANDS);
-		tag(Tags.Biomes.IS_COLD).add(BiomeKeys.SNOWY_FOREST, BiomeKeys.GLACIER);
-		tag(Tags.Biomes.IS_WASTELAND).add(BiomeKeys.GLACIER, BiomeKeys.THORNLANDS, BiomeKeys.FINAL_PLATEAU);
-		tag(Tags.Biomes.IS_DRY).add(BiomeKeys.THORNLANDS, BiomeKeys.FINAL_PLATEAU);
-		tag(Tags.Biomes.IS_PLATEAU).add(BiomeKeys.FINAL_PLATEAU);
-		tag(Tags.Biomes.IS_UNDERGROUND).add(BiomeKeys.UNDERGROUND);
+//		tag(Tags.Biomes.IS_DENSE).add(BiomeKeys.DENSE_FOREST, BiomeKeys.DENSE_MUSHROOM_FOREST, BiomeKeys.DARK_FOREST, BiomeKeys.DARK_FOREST_CENTER, BiomeKeys.SNOWY_FOREST, BiomeKeys.THORNLANDS);
+//		tag(Tags.Biomes.IS_SPARSE).add(BiomeKeys.CLEARING, BiomeKeys.OAK_SAVANNAH, BiomeKeys.GLACIER, BiomeKeys.FINAL_PLATEAU);
+//		tag(Tags.Biomes.IS_PLAINS).add(BiomeKeys.CLEARING);
+//		tag(Tags.Biomes.IS_MUSHROOM).add(BiomeKeys.MUSHROOM_FOREST, BiomeKeys.DENSE_MUSHROOM_FOREST);
+//		tag(Tags.Biomes.IS_RARE).add(BiomeKeys.ENCHANTED_FOREST, BiomeKeys.SPOOKY_FOREST, BiomeKeys.CLEARING, BiomeKeys.DENSE_MUSHROOM_FOREST, BiomeKeys.LAKE);
+//		tag(Tags.Biomes.IS_WATER).add(BiomeKeys.LAKE, BiomeKeys.STREAM);
+//		tag(Tags.Biomes.IS_MAGICAL).add(BiomeKeys.ENCHANTED_FOREST, BiomeKeys.DARK_FOREST_CENTER);
+//		tag(Tags.Biomes.IS_SPOOKY).add(BiomeKeys.SPOOKY_FOREST, BiomeKeys.DARK_FOREST, BiomeKeys.DARK_FOREST_CENTER);
+//		tag(Tags.Biomes.IS_DEAD).add(BiomeKeys.SPOOKY_FOREST, BiomeKeys.THORNLANDS, BiomeKeys.FINAL_PLATEAU);
+//		tag(Tags.Biomes.IS_SWAMP).add(BiomeKeys.SWAMP, BiomeKeys.FIRE_SWAMP);
+//		tag(Tags.Biomes.IS_SNOWY).add(BiomeKeys.SNOWY_FOREST);
+//		tag(Tags.Biomes.IS_CONIFEROUS).add(BiomeKeys.SNOWY_FOREST, BiomeKeys.HIGHLANDS);
+//		tag(Tags.Biomes.IS_COLD).add(BiomeKeys.SNOWY_FOREST, BiomeKeys.GLACIER);
+//		tag(Tags.Biomes.IS_WASTELAND).add(BiomeKeys.GLACIER, BiomeKeys.THORNLANDS, BiomeKeys.FINAL_PLATEAU);
+//		tag(Tags.Biomes.IS_DRY).add(BiomeKeys.THORNLANDS, BiomeKeys.FINAL_PLATEAU);
+//		tag(Tags.Biomes.IS_PLATEAU).add(BiomeKeys.FINAL_PLATEAU);
+//		tag(Tags.Biomes.IS_UNDERGROUND).add(BiomeKeys.UNDERGROUND);
 
 		//other vanilla tags
 		tag(BiomeTags.WITHOUT_WANDERING_TRADER_SPAWNS).addTag(IS_TWILIGHT);
