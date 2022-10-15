bb1860b79b47f4122648bc951b6cbc82fe5af323
jodi / jodlodi <jodlodi090@gmail.com>
2022-10-16 01:19:57 +0200

    Package info

---
+++ src/main/java/twilightforest/loot/modifiers/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package twilightforest.loot.modifiers;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
