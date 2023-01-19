3d847734b8274475e2226b8550b713f6984ec603
jodi / jodlodi <jodlodi090@gmail.com>
2023-01-19 19:45:57 +0100

    Slight optimizations in miscEvent class
    Less needless method calls and casts

--- src/main/java/twilightforest/events/MiscEvents.java
+++ src/main/java/twilightforest/events/MiscEvents.java
@@ -26,13 +26,12 @@ public class MiscEvents {
 
 	@SubscribeEvent
 	public static void addPrey(EntityJoinLevelEvent event) {
-		Entity entity = event.getEntity();
-		EntityType<?> type = entity.getType();
-		if (entity instanceof Mob mob) {
+		if (event.getEntity() instanceof Mob mob) {
+			EntityType<?> type = mob.getType();
 			if (type == EntityType.CAT) {
-				mob.targetSelector.addGoal(1, new NonTameRandomTargetGoal<>((TamableAnimal) entity, DwarfRabbit.class, false, null));
-				mob.targetSelector.addGoal(1, new NonTameRandomTargetGoal<>((TamableAnimal) entity, Squirrel.class, false, null));
-				mob.targetSelector.addGoal(1, new NonTameRandomTargetGoal<>((TamableAnimal) entity, TinyBird.class, false, null));
+				mob.targetSelector.addGoal(1, new NonTameRandomTargetGoal<>((TamableAnimal) mob, DwarfRabbit.class, false, null));
+				mob.targetSelector.addGoal(1, new NonTameRandomTargetGoal<>((TamableAnimal) mob, Squirrel.class, false, null));
+				mob.targetSelector.addGoal(1, new NonTameRandomTargetGoal<>((TamableAnimal) mob, TinyBird.class, false, null));
 			} else if (type == EntityType.OCELOT) {
 				mob.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(mob, DwarfRabbit.class, false));
 				mob.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(mob, Squirrel.class, false));
@@ -41,9 +40,9 @@ public class MiscEvents {
 				mob.targetSelector.addGoal(6, new NearestAttackableTargetGoal<>(mob, DwarfRabbit.class, false));
 				mob.targetSelector.addGoal(6, new NearestAttackableTargetGoal<>(mob, Squirrel.class, false));
 			} else if (type == EntityType.WOLF) {
-				mob.targetSelector.addGoal(7, new NonTameRandomTargetGoal<>((TamableAnimal) entity, DwarfRabbit.class, false, null));
-				mob.targetSelector.addGoal(7, new NonTameRandomTargetGoal<>((TamableAnimal) entity, Squirrel.class, false, null));
-				mob.targetSelector.addGoal(7, new NonTameRandomTargetGoal<>((TamableAnimal) entity, Bighorn.class, false, null));
+				mob.targetSelector.addGoal(7, new NonTameRandomTargetGoal<>((TamableAnimal) mob, DwarfRabbit.class, false, null));
+				mob.targetSelector.addGoal(7, new NonTameRandomTargetGoal<>((TamableAnimal) mob, Squirrel.class, false, null));
+				mob.targetSelector.addGoal(7, new NonTameRandomTargetGoal<>((TamableAnimal) mob, Bighorn.class, false, null));
 			}
 		}
 	}
@@ -51,8 +50,8 @@ public class MiscEvents {
 	@SubscribeEvent
 	public static void armorChanged(LivingEquipmentChangeEvent event) {
 		LivingEntity living = event.getEntity();
-		if (!living.getLevel().isClientSide() && living instanceof ServerPlayer) {
-			TFAdvancements.ARMOR_CHANGED.trigger((ServerPlayer) living, event.getFrom(), event.getTo());
+		if (living instanceof ServerPlayer serverPlayer && !serverPlayer.level.isClientSide()) {
+			TFAdvancements.ARMOR_CHANGED.trigger(serverPlayer, event.getFrom(), event.getTo());
 		}
 
 		// from what I can see, vanilla doesnt have a hook for this in the item class. So this will have to do.
@@ -65,10 +64,8 @@ public class MiscEvents {
 			}
 		}
 
-		if (event.getSlot() == EquipmentSlot.HEAD && event.getTo().is(TFBlocks.CICADA.get().asItem())) {
-			if (!event.getEntity().getLevel().isClientSide() && event.getEntity() != null) {
-				TFPacketHandler.CHANNEL.send(PacketDistributor.TRACKING_ENTITY_AND_SELF.with(event::getEntity), new CreateMovingCicadaSoundPacket(event.getEntity().getId()));
-			}
+		if (event.getEntity() != null && !living.level.isClientSide() && event.getSlot() == EquipmentSlot.HEAD && event.getTo().is(TFBlocks.CICADA.get().asItem())) {
+			TFPacketHandler.CHANNEL.send(PacketDistributor.TRACKING_ENTITY_AND_SELF.with(event::getEntity), new CreateMovingCicadaSoundPacket(event.getEntity().getId()));
 		}
 	}
 }
