aecf5c4f54fa037119cecc5421daed03382b4aa5
Tamaized <9671313+Tamaized@users.noreply.github.com>
2023-03-05 21:36:33 -0500

    Strip the seed value out of the dimension json generator

--- src/main/java/twilightforest/data/WorldGenerator.java
+++ src/main/java/twilightforest/data/WorldGenerator.java
@@ -1,19 +1,34 @@
 package twilightforest.data;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.mojang.serialization.DynamicOps;
+import com.mojang.serialization.Encoder;
+import com.mojang.serialization.JsonOps;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.Registry;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.data.CachedOutput;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.RegistryDataLoader;
+import net.minecraft.resources.RegistryOps;
+import net.minecraft.resources.ResourceKey;
 import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.DataPackRegistriesHooks;
 import twilightforest.TwilightForestMod;
 import twilightforest.data.tags.CustomTagGenerator;
 import twilightforest.init.*;
 import twilightforest.init.custom.WoodPalettes;
 
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
@@ -31,9 +46,17 @@ public class WorldGenerator extends DatapackBuiltinEntriesProvider {
 			.add(Registries.BIOME, TFBiomes::bootstrap)
 			.add(WoodPalettes.WOOD_PALETTE_TYPE_KEY, WoodPalettes::bootstrap);
 
+	private final PackOutput output;
+	private final CompletableFuture<HolderLookup.Provider> registries;
+	private final java.util.function.Predicate<String> namespacePredicate;
+
 	// Use addProviders() instead
 	private WorldGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> provider) {
 		super(output, provider, BUILDER, Set.of("minecraft", TwilightForestMod.ID));
+		this.output = output;
+		this.registries = provider.thenApply(r -> constructRegistries(r, BUILDER));
+		Set<String> set = Set.of("minecraft", TwilightForestMod.ID);
+		this.namespacePredicate = set::contains;
 	}
 
 	public static void addProviders(boolean isServer, DataGenerator generator, PackOutput output, CompletableFuture<HolderLookup.Provider> provider, ExistingFileHelper helper) {
@@ -45,5 +68,61 @@ public class WorldGenerator extends DatapackBuiltinEntriesProvider {
 
 	private static HolderLookup.Provider append(HolderLookup.Provider original, RegistrySetBuilder builder) {
 		return builder.buildPatch(RegistryAccess.fromRegistryOfRegistries(BuiltInRegistries.REGISTRY), original);
+	}
+
+	// Everything below is to strip the seed from the json
+
+	@SuppressWarnings({"UnstableApiUsage", "SameParameterValue"})
+	private static HolderLookup.Provider constructRegistries(HolderLookup.Provider original, RegistrySetBuilder datapackEntriesBuilder) {
+		var builderKeys = new HashSet<>(datapackEntriesBuilder.getEntryKeys());
+		DataPackRegistriesHooks.getDataPackRegistriesWithDimensions().filter(data -> !builderKeys.contains(data.key())).forEach(data -> datapackEntriesBuilder.add(data.key(), context -> {}));
+		return datapackEntriesBuilder.buildPatch(RegistryAccess.fromRegistryOfRegistries(BuiltInRegistries.REGISTRY), original);
+	}
+
+	@SuppressWarnings("UnstableApiUsage")
+	@Override
+	public CompletableFuture<?> run(CachedOutput p_255785_) {
+		return this.registries.thenCompose((p_256533_) -> {
+			DynamicOps<JsonElement> dynamicops = RegistryOps.create(JsonOps.INSTANCE, p_256533_);
+			return CompletableFuture.allOf(net.minecraftforge.registries.DataPackRegistriesHooks.getDataPackRegistriesWithDimensions().flatMap((p_256552_) -> {
+				return this.dumpRegistryCap(p_255785_, p_256533_, dynamicops, p_256552_).stream();
+			}).toArray((p_255809_) -> {
+				return new CompletableFuture<?>[p_255809_];
+			}));
+		});
+	}
+
+	private <T> Optional<CompletableFuture<?>> dumpRegistryCap(CachedOutput p_256502_, HolderLookup.Provider p_256492_, DynamicOps<JsonElement> p_256000_, RegistryDataLoader.RegistryData<T> p_256449_) {
+		ResourceKey<? extends Registry<T>> resourcekey = p_256449_.key();
+		return p_256492_.lookup(resourcekey).map((p_255847_) -> {
+			PackOutput.PathProvider packoutput$pathprovider = this.output.createPathProvider(PackOutput.Target.DATA_PACK, net.minecraftforge.common.ForgeHooks.prefixNamespace(resourcekey.location()));
+			return CompletableFuture.allOf(p_255847_.listElements().filter(holder -> this.namespacePredicate.test(holder.key().location().getNamespace())).map((p_256105_) -> {
+				return dumpValue(packoutput$pathprovider.json(p_256105_.key().location()), p_256502_, p_256000_, p_256449_.elementCodec(), p_256105_.value());
+			}).toArray((p_256279_) -> {
+				return new CompletableFuture<?>[p_256279_];
+			}));
+		});
+	}
+
+	private static <E> CompletableFuture<?> dumpValue(Path p_255678_, CachedOutput p_256438_, DynamicOps<JsonElement> p_256127_, Encoder<E> p_255938_, E p_256590_) {
+		Optional<JsonElement> optional = p_255938_.encodeStart(p_256127_, p_256590_).resultOrPartial((p_255999_) -> {
+			TwilightForestMod.LOGGER.error("Couldn't serialize element {}: {}", p_255678_, p_255999_);
+		});
+		if (p_255678_.endsWith("twilight_forest.json"))
+			optional.ifPresent(WorldGenerator::stripSeedRecursive);
+		return optional.isPresent() ? DataProvider.saveStable(p_256438_, optional.get(), p_255678_) : CompletableFuture.completedFuture((Object)null);
+	}
+
+	private static void stripSeedRecursive(JsonElement element) {
+		if (element.isJsonArray()) {
+			element.getAsJsonArray().forEach(WorldGenerator::stripSeedRecursive);
+		} else if (element.isJsonObject()) {
+			JsonObject next = element.getAsJsonObject();
+			if (next.has("seed")) {
+				TwilightForestMod.LOGGER.debug("Removing Seed from: {}", next);
+				next.remove("seed");
+			}
+			element.getAsJsonObject().entrySet().forEach(e -> stripSeedRecursive(e.getValue()));
+		}
 	}
 }
