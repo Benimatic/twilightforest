e9e6e48a7a54fb55ce212a7da795ae6b5ff0f95f
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2023-07-03 22:08:57 -0600

    fixed chain hitboxes being way too big

--- src/main/java/twilightforest/entity/Chain.java
+++ src/main/java/twilightforest/entity/Chain.java
@@ -3,17 +3,24 @@ package twilightforest.entity;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityDimensions;
+import net.minecraft.world.entity.Pose;
 import twilightforest.entity.monster.BlockChainGoblin;
 
 public class Chain extends BlockChainGoblin.MultipartGenericsAreDumb {
 
 	public Chain(Entity parent) {
 		super(parent);
+		this.realSize = EntityDimensions.scalable(0.25F, 0.25F);
+	}
+
+	@Override
+	public EntityDimensions getDimensions(Pose pos) {
+		return EntityDimensions.scalable(0.25F, 0.25F);
 	}
 
 	@Override
 	protected void defineSynchedData() {
-		this.realSize = EntityDimensions.scalable(0.75F, 0.75F);
+
 	}
 
 	@Override
