b074023951a4bf266e6563e7778ca0abd7e0da73
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2023-07-30 18:59:25 -0600

    fixed #1853

--- src/main/java/twilightforest/inventory/slot/UncraftingResultSlot.java
+++ src/main/java/twilightforest/inventory/slot/UncraftingResultSlot.java
@@ -10,12 +10,16 @@ import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeType;
 import twilightforest.inventory.UncraftingContainer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UncraftingResultSlot extends ResultSlot {
 
 	private final Player player;
 	private final Container inputSlot;
 	private final UncraftingContainer uncraftingMatrix;
 	private final CraftingContainer assemblyMatrix;
+	private final Map<Integer, ItemStack> tempRemainderMap;
 
 	public UncraftingResultSlot(Player player, Container input, Container uncraftingMatrix, Container assemblyMatrix, Container result, int slotIndex, int x, int y) {
 		super(player, (CraftingContainer) assemblyMatrix, result, slotIndex, x, y);
@@ -23,12 +27,16 @@ public class UncraftingResultSlot extends ResultSlot {
 		this.inputSlot = input;
 		this.uncraftingMatrix = (UncraftingContainer) uncraftingMatrix;
 		this.assemblyMatrix = (CraftingContainer) assemblyMatrix;
+		this.tempRemainderMap = new HashMap<>();
 	}
 
 	@Override
 	public void onTake(Player player, ItemStack stack) {
 		// let's see, if the assembly matrix can produce this item, then it's a normal recipe, if not, it's combined.  Will that work?
 		boolean combined = true;
+
+		//clear the temp map, just in case
+		this.tempRemainderMap.clear();
 
 		for (Recipe<CraftingContainer> recipe : player.level().getRecipeManager().getRecipesFor(RecipeType.CRAFTING, this.assemblyMatrix, this.player.level())) {
 			if (ItemStack.isSameItemSameTags(recipe.getResultItem(player.level().registryAccess()), stack)) {
@@ -44,8 +52,13 @@ public class UncraftingResultSlot extends ResultSlot {
 			}
 
 			// if we are using a combined recipe, wipe the uncrafting matrix and decrement the input appropriately
-			for (int i = 0; i < uncraftingMatrix.getContainerSize(); i++) {
-				this.uncraftingMatrix.setItem(i, ItemStack.EMPTY);
+			for (int i = 0; i < this.uncraftingMatrix.getContainerSize(); i++) {
+				if (this.assemblyMatrix.getItem(i).isEmpty()) {
+					this.uncraftingMatrix.setItem(i, ItemStack.EMPTY);
+				} else {
+					//if we have an ingredient in the grid and one in the uncrafting matrix, copy the uncrafting matrix item for later
+					this.tempRemainderMap.put(i, this.uncraftingMatrix.getItem(i));
+				}
 			}
 			this.inputSlot.removeItem(0, this.uncraftingMatrix.numberOfInputItems);
 		}
@@ -72,6 +85,10 @@ public class UncraftingResultSlot extends ResultSlot {
 					this.player.drop(remainingStack, false);
 				}
 			}
+		}
+		//add all remainders to the crafting grid. This prevents any extra items from being deleted during the recrafting process.
+		if (!this.tempRemainderMap.isEmpty()) {
+			this.tempRemainderMap.forEach(this.assemblyMatrix::setItem);
 		}
 	}
 }
